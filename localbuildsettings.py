c32e4a23cc44dabc27e2caf462ac3a30958e7c8f
johndoe <johnd0e@mail.ua>
2019-01-13 15:02:04 +0200

    deploy: fix update urls (sample)

---
+++ localbuildsettings.py
@@ -0,0 +1,13 @@
+buildSettings = {
+    # release: using to deploy release builds at official iitc-ce site
+    'release': {
+        'resourceUrlBase': 'https://iitc.modos189.ru/build/release/',
+        'distUrlBase': 'https://iitc.modos189.ru/build/release/',
+    },
+
+    # test: using to deploy test builds at official iitc-ce site
+    'test': {
+        'resourceUrlBase': 'https://iitc.modos189.ru/build/test/',
+        'distUrlBase': 'https://iitc.modos189.ru/build/test/',
+    },
+}
