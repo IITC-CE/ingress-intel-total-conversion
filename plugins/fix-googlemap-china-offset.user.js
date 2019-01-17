c1e4cf085fe75d07eb5165ef49b6fb959fc67356
johndoe <johnd0e@mail.ua>
2019-01-17 17:56:27 +0200

    fixup! fix-googlemap-china-offset: refactoring

---
+++ plugins/fix-googlemap-china-offset.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @id             iitc-plugin-fix-googlemap-china-offset@breezewish
+// @name           IITC plugin: Fix Google Map offsets in China
+// @category       Deleted
+// @version        0.0.1.@@DATETIMEVERSION@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Deprecated, use fix-china-offset instead
+@@METAINFO@@
+// ==/UserScript==
