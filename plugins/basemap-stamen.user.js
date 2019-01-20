aaab9778032f84f0510358692640d1e1cc753500
johndoe <johnd0e@mail.ua>
2019-01-20 23:34:48 +0200

    basemap-stamen: minor improvements
    * correct attribution
    * add some layers //commented
    * refactor (for the sake of perfection)

--- plugins/basemap-stamen.user.js
+++ plugins/basemap-stamen.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-basemap-stamen@jonatkins
 // @name           IITC plugin: Map layers from stamen.com
 // @category       Map Tiles
-// @version        0.2.0.@@DATETIMEVERSION@@
+// @version        0.2.1.@@DATETIMEVERSION@@
 // @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add the 'Toner' and 'Watercolor' map layers from maps.stamen.com.
 @@METAINFO@@
 // ==/UserScript==
@@ -12,49 +12,63 @@
 // PLUGIN START ////////////////////////////////////////////////////////
 
 
-// use own namespace for plugin
-window.plugin.mapTileStamen = function() {};
+// see API here http://maps.stamen.com/
+// https://stamen-maps.a.ssl.fastly.net/js/tile.stamen.js (overcomplicated)
 
+function setup () {
+  var baseUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/{layer}/{z}/{x}/{y}.{type}';
+  var L_StamenTileLayer = L.TileLayer.extend({
+    options: {
+      subdomains: 'abcd',
+      type: 'png',
+      minZoom: 0,
+      maxZoom: 21,
+      attribution: [
+        'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+      ].join('')
+    },
+    initialize: function (name, options) {
+      options.layer = name.replace(' ','-').toLowerCase();
+      L.TileLayer.prototype.initialize.call(this, baseUrl, options);
+    }
+  });
 
-window.plugin.mapTileStamen.addLayer = function() {
-
-  var types = {
-    'toner': [ 'Toner', 'png', 0, 20 ],
-//    'toner-hybrid': [ ' Toner Hybrid', 'png', 0, 20 ],  // transparent layer. could be useful over satellite imagery or similar
-//    'toner-labels': [ 'Toner Labels', 'png', 0, 20 ],  // transparent layer. could be useful over satellite imagery or similar
-//    'toner-lines': [ 'Toner Lines', 'png', 0, 20 ],  // transparent layer. could be useful over satellite imagery or similar
-    'toner-background': [ 'Toner Background', 'png', 0, 20 ],
-    'toner-lite': [ 'Toner Lite', 'png', 0, 20 ],
-    'watercolor': [ 'Watercolor', 'jpg', 1, 16 ],
-  };
-
-  var baseUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/';
-
-
-  for (var layer in types) {
-    var info = types[layer];
-
-    var name = info[0];
-    var type = info[1];
-    var minZoom = info[2];
-    var maxZoom = info[3];
-
-    var mapLayer = new L.TileLayer (baseUrl+'{layer}/{z}/{x}/{y}.{type}', {
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-      subdomains: 'abcd',    
-      layer: layer,
-      type: type,
-      minZoom: minZoom,
-      maxNativeZoom: maxZoom,
-      maxZoom: 21
-    });
-
-    layerChooser.addBaseLayer(mapLayer,'Stamen '+name);
+  function addLayer (name,options) {
+    layerChooser.addBaseLayer(new L_StamenTileLayer(name,options),'Stamen ' + name);
   }
 
-};
+  var options = { minZoom: 0, maxNativeZoom: 20 };
+  addLayer('Toner',options);
+  addLayer('Toner Background',options);
+  addLayer('Toner Lite',options);
+  // transparent layers. could be useful over satellite imagery or similar
+  // addLayer('Toner Hybrid',options);
+  // addLayer('Toner Labels',options);
+  // addLayer('Toner Lines',options);
 
-var setup =  window.plugin.mapTileStamen.addLayer;
+  options = { minZoom: 1, maxNativeZoom: 13 }; // Should support up to 18, but too many 404 on zoom > 13
+  // addLayer('Terrain',options);
+  // addLayer('Terrain Labels',options);
+  // addLayer('Terrain Lines',options);
+  // addLayer('Terrain Background',options);
+
+  options = {
+    minZoom: 1,
+    maxZoom: 21,
+    maxNativeZoom: 18,
+    type: 'jpg',
+    attribution: [
+      'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
+      'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+      'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+      'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    ].join('')
+  };
+  addLayer('Watercolor',options);
+}
 
 // PLUGIN END //////////////////////////////////////////////////////////
 
