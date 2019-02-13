3ef0a38a02183eaee17d7c829495935bf27aaa0d
johndoe <johnd0e@mail.ua>
2019-02-13 15:03:25 +0200

    leaflet: use @@INCLUDECSS macro

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ if (typeof(PLAYER)!="object" || typeof(PLAYER.nickname) != "string") {
 document.getElementsByTagName('head')[0].innerHTML = ''
   + '<title>Ingress Intel Map</title>'
   + '<style>@@INCLUDESTRING:style.css@@</style>'
-  + '<style>@@INCLUDESTRING:external/leaflet.css@@</style>'
+  + '<style>@@INCLUDECSS:external/leaflet.css@@</style>'
 //note: smartphone.css injection moved into code/smartphone.js
   + '<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic&subset=latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic"/>';
 
