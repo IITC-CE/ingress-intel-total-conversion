23681098fdaea0bb11bc774485c17916c11dd8e2
johndoe <johnd0e@mail.ua>
2019-10-29 14:43:23 +0200

    plugins/deleted: remove old stubs

--- plugins/deleted/basemap-nokia-ovi.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @id             iitc-plugin-nokia-ovi-maps
-// @name           IITC plugin: Nokia OVI maps
-// @category       Deleted
-// @version        0.1.3.@@DATETIMEVERSION@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Nokia OVI Maps was closed.
-@@METAINFO@@
-// ==/UserScript==
--- plugins/deleted/fix-googlemap-china-offset.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @id             iitc-plugin-fix-googlemap-china-offset@breezewish
-// @name           IITC plugin: Fix Google Map offsets in China
-// @category       Deleted
-// @version        0.0.1.@@DATETIMEVERSION@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Deprecated, use fix-china-map-offset instead
-@@METAINFO@@
-// ==/UserScript==
