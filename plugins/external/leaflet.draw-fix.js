ee2a49eae7f66c2a6db176c5a251b5fc98510868
johndoe <johnd0e@mail.ua>
2020-01-24 10:49:54 +0200

    leaflet.draw-fix.js: remove a patch as it's not needed with latest Leaflet
    https://github.com/Leaflet/Leaflet.draw/issues/922#issuecomment-578035240

--- plugins/external/leaflet.draw-fix.js
+++ plugins/external/leaflet.draw-fix.js
@@ -1,24 +1,3 @@
-// https://github.com/IITC-CE/ingress-intel-total-conversion/pull/175
-// https://github.com/Leaflet/Leaflet.draw/issues/922
-
-(function (addHooks,removeHooks) {
-L.Draw.SimpleShape.include({
-  addHooks: function () {
-    addHooks.call(this);
-    if (this._map) {
-      document.removeEventListener('touchstart', L.DomEvent.preventDefault);
-      this._map.getPanes().mapPane.addEventListener('touchstart', L.DomEvent.preventDefault, {passive: false});
-    }
-  },
-  removeHooks: function () {
-    removeHooks.call(this);
-    if (this._map) {
-      this._map.getPanes().mapPane.removeEventListener('touchstart', L.DomEvent.preventDefault);
-    }
-  }
-});
-})(L.Draw.SimpleShape.prototype.addHooks,L.Draw.SimpleShape.prototype.removeHooks);
-
 // https://github.com/Leaflet/Leaflet.draw/issues/789
 // https://github.com/Leaflet/Leaflet.draw/issues/695
 L.Draw.Polyline.prototype._onTouch_original = L.Draw.Polyline.prototype._onTouch; // just in case
