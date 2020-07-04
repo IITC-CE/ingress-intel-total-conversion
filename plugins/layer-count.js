cddb1926d2ed33ae58205624686f4f6e2f8ba4a4
Alexander Danilov <git@modos189.ru>
2020-07-05 01:54:49 +0300

    Replacement of own implementation of triangle area calculation with Leaflet.draw library

--- plugins/layer-count.js
+++ plugins/layer-count.js
@@ -64,123 +64,12 @@ plugin.layerCount.pnpoly = function(latlngs, point) {
 	return c;
 }
 
-/***********************************************************************
- * Code for calculating the area of a triangle on the surface of a
- * sphere.  See in-line comments for references.
- **********************************************************************/
-// Compute hav(theta), as per https://en.wikipedia.org/wiki/Haversine_formula
-plugin.layerCount.havTheta = function(theta) {
-	return (1 - Math.cos(theta)) / 2;
-};
-
-plugin.layerCount.deg2Radians= function(degrees) {
-	return degrees / 360 * 2 * Math.PI;
-};
-
-// Compute hav(Theta) (NB: Capital theta!) as per https://en.wikipedia.org/wiki/Haversine_formula
-plugin.layerCount.haversine = function(LatLngPair) {
-  let havLat = plugin.layerCount.havTheta(plugin.layerCount.deg2Radians(LatLngPair[1]['lat'] - LatLngPair[0]['lat']));
-  let cosPoint1 = Math.cos(plugin.layerCount.deg2Radians(LatLngPair[0]['lat']));
-  let cosPoint2 = Math.cos(plugin.layerCount.deg2Radians(LatLngPair[1]['lat']));
-  let havLng = plugin.layerCount.havTheta(plugin.layerCount.deg2Radians(LatLngPair[1]['lng'] - LatLngPair[0]['lng']));
-	return havLat + cosPoint1 * cosPoint2 * havLng;
-};
-
-
-/*
- * Calculate the Great Circle Distance (not Greatest Common Denominator!)
- * of a pair of Latitude/Longitude points.
- * Ref: https://en.wikipedia.org/wiki/Haversine_formula
- */
-plugin.layerCount.calcGCD= function(LatLngPair) {
-	let haverSine = plugin.layerCount.haversine(LatLngPair);
-	return 2 * Math.asin(Math.sqrt(haverSine));
-};
-
-/*
- * For a set of three Latitude/Longitude points calculate the area of
- * the enclosed triangle.
- * Ref: https://en.wikipedia.org/wiki/Spherical_trigonometry#Area_and_spherical_excess
- *
- * We're using an average value of the Earth's radius, and treating the
- * Earth as a perfect sphere. Thus this calculation will only yield an
- * approximation, with the error varying by latitude.
- */
+// Code for calculating the area of a triangle on the surface of a sphere.
 plugin.layerCount.fieldarea = function(LatLngs) {
 	if (LatLngs.length !== 3) {
 		return 0.0;
 	}
-	let radiusEarth = 6371.0 * 1000.0; // In metres
-	// NB: All initial work is done on a unit sphere, radius = 1
-	//     We'll multiply up versus the surface area of the Earth
-	//     at the end.
-	// Use Haversine
-	// https://en.wikipedia.org/wiki/Haversine_formula
-	// to calculate the lengths of the sides of the field, storing
-	// them in d[]
-	//
-	// As per https://en.wikipedia.org/wiki/Spherical_trigonometry#Notation
-	// [A,B,C] are the angles in the triangle. These correspdond to
-	// [u,v,w] the vertices at the corners.
-	// [a,b,c] are the edges, where:
-	// 	a = v - u
-	// 	b = u - w
-	// 	c = w - v
-	//
-	// d[0] = a = v - u
-	// d[1] = b = u - w
-	// d[2] = c = w - v
-	let d = [];
-	for (let gcd = 0; gcd < 3 ; gcd++) {
-		let p2 = (4 - gcd) % 3;
-    let p1 = (3 - gcd) % 3;
-
-		d[gcd] = plugin.layerCount.calcGCD([LatLngs[p1], LatLngs[p2]]);
-	}
-	
-	// Now we need the internal angle, using the GCDs, between
-	// side a and side b, which is the angle C.
-	// https://en.wikipedia.org/wiki/Spherical_law_of_cosines#Rearrangements
-	let cosC = ( Math.cos(d[2]) - (Math.cos(d[0]) * Math.cos(d[1])) ) / ( Math.sin(d[0]) * Math.sin(d[1]) );
-  let C = Math.acos(cosC);
-
-	// From this we calculate the Spherical Excess 'E', which is directly
-	// related to the area of the enclosed triangle.
-	let E = 2 * Math.atan(
-			(Math.tan(d[0]/2)
-			 * Math.tan(d[1]/2)
-			 * Math.sin(C)
-			) / (1
-			 + Math.tan(d[0]/2)
-			  * Math.tan(d[1]/2)
-			  * Math.cos(C)
-			)
-		);
-	
-	// https://en.wikipedia.org/wiki/Spherical_trigonometry#Area_and_spherical_excess
-	//
-	// 1) E = A + B + C - pi
-	// 2) A + B + C  = pi + (4 * pi * areaTriangle) / areaSphere
-	//
-	// Pull the pi to the left in #2:
-	//
-	// 3) A + B + C - pi = (4 * pi * areaTriangle) / areaSphere
-	//
-	// LHS is now the same as RHS in #1, so = E:
-	//
-	//    E = (4 * pi * areaTriangle) / areaSphere
-	//    E * areaSphere = 4 * pi * areaTriangle
-	//    (E * areaSphere) / (4 * pi) = areaTriangle
-	//    areaTriangle = (E * areaSphere) / (4 * pi)
-	//
-	// So, using Earth's surface area as areaSphere
-	//
-	//    areaOnEarth = E * (4 * pi * Earth['radius']^2) / (4 * pi)
-	//
-	// and the "4 * pi" cancels out either side:
-	//
-	//    areaOnEarth = E * Earth['radius']
-  return E * Math.pow(radiusEarth, 2)
+  return L.GeometryUtil.geodesicArea(LatLngs)
 };
 
 // Take an area in m² and output a not-too long string
@@ -197,9 +86,6 @@ plugin.layerCount.prettyAreaString = function(area) {
 		return (a / 1000.0 / 1000.0).toLocaleString() + " km²";
 	}
 };
-/***********************************************************************
- * End triangle area code
- **********************************************************************/
 
 plugin.layerCount.calculate = function(ev) {
 	var point = ev.latlng;
@@ -257,6 +143,9 @@ plugin.layerCount.calculate = function(ev) {
 };
 
 var setup = function() {
+  if (window.plugin.drawTools === undefined) {
+    '@include_raw:external/leaflet.draw-src.js@';
+  }
 	$('<style>').prop('type', 'text/css').html('@include_string:layer-count.css@').appendTo('head');
 
 	var parent = $(".leaflet-top.leaflet-left", window.map.getContainer());
