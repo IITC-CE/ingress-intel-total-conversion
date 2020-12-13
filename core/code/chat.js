2fad7bc5ab7b49b4102c0757fe8f66261cf983b0
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-12-13 19:02:26 +0100

    chat: move oldest/newest msg update

--- core/code/chat.js
+++ core/code/chat.js
@@ -316,7 +316,7 @@ window.chat.nicknameClicked = function(event, nickname) {
   return false;
 }
 
-window.chat.writeDataToHash = function(newData, storageHash, isPublicChannel, isOlderMsgs, isAscendingOrder) {
+window.chat.updateOldNewHash = function(newData, storageHash, isOlderMsgs, isAscendingOrder) {
   // track oldest + newest timestamps/GUID
   if (newData.result.length > 0) {
     var first = {
@@ -345,6 +345,11 @@ window.chat.writeDataToHash = function(newData, storageHash, isPublicChannel, is
       }
     }
   }
+}
+
+window.chat.writeDataToHash = function(newData, storageHash, isPublicChannel, isOlderMsgs, isAscendingOrder) {
+  window.chat.updateOldNewHash(newData, storageHash, isOldMsgs, isAscendingOrder);
+
   $.each(newData.result, function(ind, json) {
     // avoid duplicates
     if(json[0] in storageHash.data) return true;
