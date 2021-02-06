bc06071fda8dfdac50261663389ce8c169bc5988
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-02-06 21:52:15 +0100

    preserve history if possible

--- core/code/map_data_render.js
+++ core/code/map_data_render.js
@@ -306,17 +306,19 @@ window.Render.prototype.createPortalEntity = function(ent) {
     this.deletePortalEntity(ent[0]);
   }
 
-  var portalLevel = parseInt(ent[2][4])||0;
-  var team = teamStringToId(ent[2][1]);
+  var data = decodeArray.portalSummary(ent[2]);
+
+  var portalLevel = parseInt(data.level)||0;
+  var team = teamStringToId(data.team);
   // the data returns unclaimed portals as level 1 - but IITC wants them treated as level 0
   if (team == TEAM_NONE) portalLevel = 0;
 
-  var latlng = L.latLng(ent[2][2]/1E6, ent[2][3]/1E6);
+  var latlng = L.latLng(data.latE6/1E6, data.lngE6/1E6);
 
-  var data = decodeArray.portalSummary(ent[2]);
- 
-  var history = ent[2][18];
- 
+  // preserve history (bitwise or)
+  if (previousData && previousData.history)
+    data.history |= previousData.history;
+
   var dataOptions = {
     level: portalLevel,
     team: team,
@@ -324,7 +326,6 @@ window.Render.prototype.createPortalEntity = function(ent) {
     guid: ent[0],
     timestamp: ent[1],
     data: data,
-    history: history
   };
 
   window.pushPortalGuidPositionCache(ent[0], data.latE6, data.lngE6);
