58d91a8dd0989179a539e35f04ee2ee58aaae89d
MysticJay <gtp7501mj@gmail.com>
2021-02-11 00:28:50 +0100

    Toggle Switch w. SVG
    Toggle-switch
    Typos

--- plugins/portal-history.js
+++ plugins/portal-history.js
@@ -294,12 +294,16 @@ window.plugin.portalHistory.toggleHistory = function(keepUIbutton) {
 var setup = function() {
   window.addPortalHighlighter('History: visited/captured', window.plugin.portalHistory.visited);
   window.addPortalHighlighter('History: NOT visited/captured', window.plugin.portalHistory.notVisited);
-  window.addPortalHighlighter('History: scoutControlled', window.plugin.portalHistory.scoutControlled);
-  window.addPortalHighlighter('History: NOT scoutControlled', window.plugin.portalHistory.notScoutControlled);
+  window.addPortalHighlighter('History: Scout controlled', window.plugin.portalHistory.scoutControlled);
+  window.addPortalHighlighter('History: NOT Scout controlled', window.plugin.portalHistory.notScoutControlled);
   window.plugin.portalHistory.setupLayers();
   if (window.plugin.portalslist) {
     window.plugin.portalHistory.setupPortalsList();
   }
+
+  var checkedCircle = '<svg class="tracker-eye" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"> <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"/></svg>';
+  var emptyCircle =  '<svg class="tracker-eye" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"> <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z"/></svg>';
+
   var style = `
   <style>
     #toggleHistory {
@@ -308,11 +312,11 @@ var setup = function() {
     }
 
     #toggleHistory.normHistory {
-      background-color: red;
+      background-image: url('data:image/svg+xml;charset=UTF8,`+checkedCircle+`');
     }
 
     #toggleHistory.revHistory {
-      background-color: black;
+      background-image: url('data:image/svg+xml;charset=UTF8,`+emptyCircle+`');
     }
   </style>
   `;
