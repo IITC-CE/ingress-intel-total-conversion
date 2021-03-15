ac0796494676824f2dfc3bc6d388fde692ed3935
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-03-15 22:08:15 +0100

    Revert "score-cycle-times: use 24h format"
    This reverts commit 37b48d34916a01c81fcd93a4efa521242db7298b.

--- plugins/score-cycle-times.js
+++ plugins/score-cycle-times.js
@@ -42,8 +42,8 @@ window.plugin.scoreCycleTimes.update = function() {
 
 
   var formatRow = function(label,time) {
-    var timeStr = unixTimeToDateTimeString(time,false);
-    timeStr = timeStr.replace(/:00$/,''); 
+    var timeStr = unixTimeToString(time,true);
+    timeStr = timeStr.replace(/:00$/,''); //FIXME: doesn't remove seconds from AM/PM formatted dates
 
     return '<tr><td>'+label+'</td><td>'+timeStr+'</td></tr>';
   };
