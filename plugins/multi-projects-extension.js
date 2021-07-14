2ff5bf2798be21c9c3b4ee49067565e7bab8c8f9
McBen <viertelvor12@gmx.net>
2021-07-14 09:32:26 +0200

    MPE: Apply Eslint auto fixes

--- plugins/multi-projects-extension.js
+++ plugins/multi-projects-extension.js
@@ -23,45 +23,45 @@ window.plugin.mpe.action = {};
 window.plugin.mpe.obj.projects = {};
 window.plugin.mpe.obj.opt = {settings:{manager:[], sidebar:[]}};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // STORAGE
-//------------------------------------------------------
+// ------------------------------------------------------
 window.plugin.mpe.storage.NAME = 'plugin-mpe';
 window.plugin.mpe.storage.saveStorage = function(){
   window.localStorage[window.plugin.mpe.storage.NAME] = JSON.stringify(window.plugin.mpe.obj.opt);
-}
+};
 window.plugin.mpe.storage.loadStorage = function(){
   window.plugin.mpe.obj.opt = JSON.parse(window.localStorage[window.plugin.mpe.storage.NAME]);
-}
+};
 window.plugin.mpe.storage.checkStorage = function(){
-  if(window.localStorage[window.plugin.mpe.storage.NAME] === undefined){
+  if (window.localStorage[window.plugin.mpe.storage.NAME] === undefined){
     window.plugin.mpe.obj.opt = {settings:{manager:[], sidebar:[]}};
     window.plugin.mpe.storage.saveStorage();
   }
   window.plugin.mpe.storage.loadStorage();
-}
+};
 
 window.plugin.mpe.storage.addStorage = function(storage){
   window.localStorage[storage] = '';
-}
+};
 window.plugin.mpe.storage.removeStorage = function(storage){
   delete window.localStorage[storage];
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // DATA
-//------------------------------------------------------
+// ------------------------------------------------------
 // Format the string
 window.plugin.mpe.data.validateName = function(name){
   var name = name.toString();
   var pattern = new RegExp(/^[a-zA-Z0-9_\-\ \(\)\[\]]/);
-  for(var i=0; i<name.length; i++){
-    if(!pattern.test(name[i])){
+  for (var i=0; i<name.length; i++){
+    if (!pattern.test(name[i])){
       return false;
     }
   }
   return true;
-}
+};
 window.plugin.mpe.data.nameToField = function(PJ, name){
   var preKey = 'MPE_'+window.plugin.mpe.data.getPreKeyPj(PJ);
   var field = name;
@@ -69,7 +69,7 @@ window.plugin.mpe.data.nameToField = function(PJ, name){
   field = field.replace(/ /g, '___');
   field = preKey+field;
   return field;
-}
+};
 window.plugin.mpe.data.fieldToName = function(PJ, field){
   var preKey = 'MPE_'+window.plugin.mpe.data.getPreKeyPj(PJ);
   var name = field;
@@ -77,103 +77,103 @@ window.plugin.mpe.data.fieldToName = function(PJ, field){
   name = name.replace(preKey, '');
   name = name.replace(/___/g, ' ');
   return name;
-}
+};
 
 window.plugin.mpe.data.getCurrKeyPj = function(PJ){
   return window.plugin.mpe.obj.projects[PJ].currKey;
-}
+};
 window.plugin.mpe.data.getPreKeyPj = function(PJ){
   return window.plugin.mpe.obj.projects[PJ].defaultKey;
-}
+};
 window.plugin.mpe.data.getProjects = function(PJ){
   return window.plugin.mpe.obj.projects[PJ].pj;
-}
+};
 
 window.plugin.mpe.data.isInSidebar = function(PJ){
   var arrSidebar = window.plugin.mpe.obj.opt['settings']['sidebar'];
   var index = arrSidebar.indexOf(PJ);
-  if(index >= 0){
+  if (index >= 0){
     return index;
   }
   return -1;
-}
+};
 window.plugin.mpe.data.isInManager = function(PJ){
   var arrSidebar = window.plugin.mpe.obj.opt['settings']['manager'];
   var index = arrSidebar.indexOf(PJ);
-  if(index >= 0){
+  if (index >= 0){
     return index;
   }
   return -1;
-}
+};
 
 window.plugin.mpe.data.getFaClass = function(PJ){
   return window.plugin.mpe.obj.projects[PJ].fa;
-}
+};
 window.plugin.mpe.data.getTitle = function(PJ){
   return window.plugin.mpe.obj.projects[PJ].title;
-}
+};
 
 
 window.plugin.mpe.data.addStorageToPJ = function(PJ, storage){
   window.plugin.mpe.obj.projects[PJ].pj.push(storage);
-}
+};
 window.plugin.mpe.data.removeStorageFromPJ = function(PJ, storage){
   var index = window.plugin.mpe.obj.projects[PJ].pj.indexOf(storage);
-  if(index >= 0){ window.plugin.mpe.obj.projects[PJ].pj.splice(index, 1); }
-}
+  if (index >= 0){ window.plugin.mpe.obj.projects[PJ].pj.splice(index, 1); }
+};
 window.plugin.mpe.data.setDefaultKey = function(PJ){
   window.plugin.mpe.obj.projects[PJ].currKey = window.plugin.mpe.data.getPreKeyPj();
-}
+};
 window.plugin.mpe.data.setKey = function(PJ, keyName){
   window.plugin.mpe.obj.projects[PJ].currKey = keyName;
-}
+};
 
 window.plugin.mpe.data.scanStorageForAll = function(){
   var list = window.plugin.mpe.obj.projects;
-  for(var name in list){
+  for (var name in list){
     window.plugin.mpe.data.scanStorageForOne(name);
   }
-}
+};
 window.plugin.mpe.data.scanStorageForOne = function(name){
   var PROJ = window.plugin.mpe.obj.projects[name];
   PROJ.pj = [];
 
   //        if(window.localStorage[PROJ.defaultKey] !== undefined){
-  for(var field in window.localStorage){
-    if(field.includes('MPE_'+PROJ.defaultKey)){
+  for (var field in window.localStorage){
+    if (field.includes('MPE_'+PROJ.defaultKey)){
       PROJ.pj.push(field);
     }
   }
   //        }
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // HTML and UI
-//------------------------------------------------------
+// ------------------------------------------------------
 window.plugin.mpe.getHTML.projectOptions = function(PJ){
   var PROJ = window.plugin.mpe.obj.projects[PJ];
   var html = '';
   var active = '';
-  if(PROJ.currKey === PROJ.defaultKey){ active = 'selected'; }
+  if (PROJ.currKey === PROJ.defaultKey){ active = 'selected'; }
   html += '<option '+active+' value="'+PROJ.defaultKey+'">Default Project</option>';
 
-  for(var index in PROJ.pj){
+  for (var index in PROJ.pj){
     var nameMultiStorage = PROJ.pj[index];
     var label = nameMultiStorage.replace('MPE_'+PROJ.defaultKey, '');
 
     var active = '';
-    if(PROJ.currKey === nameMultiStorage){ active = 'selected'; }
+    if (PROJ.currKey === nameMultiStorage){ active = 'selected'; }
     html += '<option '+active+' value="'+nameMultiStorage+'">'+window.plugin.mpe.data.fieldToName(PJ, label)+'</option>';
   }
   return html;
-}
+};
 window.plugin.mpe.getHTML.project = function(PJ){
   var PROJ = window.plugin.mpe.obj.projects[PJ];
   var listElem = '';
   var txtNew = '+';
   var txtDel = 'X';
 
-  if(window.plugin.faIcon){
+  if (window.plugin.faIcon){
     txtNew = '<i class="fa fa-plus"></i>';
     txtDel = '<i class="fa fa-trash"></i>';
   }
@@ -191,15 +191,15 @@ window.plugin.mpe.getHTML.project = function(PJ){
   listElem += '</div>';
 
   return listElem;
-}
+};
 window.plugin.mpe.getHTML.projectsAll = function(){
   var html = '';
 
-  for(var name in window.plugin.mpe.obj.projects){
+  for (var name in window.plugin.mpe.obj.projects){
     html += window.plugin.mpe.getHTML.project(name);
   }
   return html;
-}
+};
 window.plugin.mpe.getHTML.prjSett = function(PJ){
   var PROJ = window.plugin.mpe.obj.projects[PJ];
   var listElem = '';
@@ -209,7 +209,7 @@ window.plugin.mpe.getHTML.prjSett = function(PJ){
   var inManager = (window.plugin.mpe.data.isInManager(PJ) < 0)? 'checked' : '';
   var inSidebar = (window.plugin.mpe.data.isInSidebar(PJ) >= 0)? 'checked' : '';
 
-  if(window.plugin.faIcon){
+  if (window.plugin.faIcon){
     txtNew = '<i class="fa fa-plus"></i>';
     txtDel = '<i class="fa fa-trash"></i>';
   }
@@ -224,46 +224,46 @@ window.plugin.mpe.getHTML.prjSett = function(PJ){
   listElem += '</div>';
 
   return listElem;
-}
+};
 window.plugin.mpe.getHTML.prjSettAll = function(){
   var html = '';
 
-  for(var type in window.plugin.mpe.obj.projects){
+  for (var type in window.plugin.mpe.obj.projects){
     html += window.plugin.mpe.getHTML.prjSett(type);
   }
   return html;
-}
+};
 
 window.plugin.mpe.ui.deleteOption = function(PJ, storageName){
   var PROJ = window.plugin.mpe.obj.projects[PJ];
-  if(storageName !== PROJ.defaultKey){
+  if (storageName !== PROJ.defaultKey){
     //            $('.mpeManager select.'+PJ+' option[value = "'+storageName+'"]').remove();
     $('.mpe[data-mpe="'+PJ+'"] select option[value = "'+storageName+'"]').remove();
   }
-}
+};
 window.plugin.mpe.ui.appendOption = function(PJ, storageName){
   var opt = '<option value="'+storageName+'">'+window.plugin.mpe.data.fieldToName(PJ, storageName)+'</option>';
   //        $('.mpeManager select.'+PJ+'').append(opt);
   $('.mpe[data-mpe="'+PJ+'"] select').append(opt);
-}
+};
 window.plugin.mpe.ui.redrawHTMLOptions = function(PJ){
   var PROJ = window.plugin.mpe.obj.projects[PJ];
   var html = window.plugin.mpe.getHTML.projectOptions(PJ);
   $('.mpe[data-mpe="'+PJ+'"] select').html(html);
-}
+};
 
 window.plugin.mpe.ui.addControl = function(){
   $('#toolbox').append('<a class="list-group-item" onclick="window.plugin.mpe.dialog.openMain();return false;"><i class="fa fa-files-o"></i>MultiProjects</a>');
-}
+};
 window.plugin.mpe.ui.appendContainerInSidebar = function(){
-  if(!$('#multi-projects.mpeSidebar').length){
+  if (!$('#multi-projects.mpeSidebar').length){
     $('#sidebar').append('<div id="multi-projects" class="mpeSidebar list-group"></div>');
   }
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // DIALOGS
-//------------------------------------------------------
+// ------------------------------------------------------
 window.plugin.mpe.dialog.openMain = function(){
   //        window.plugin.mpe.data.scanStorageForAll();
   var html = window.plugin.mpe.getHTML.projectsAll();
@@ -274,17 +274,17 @@ window.plugin.mpe.dialog.openMain = function(){
     dialogClass: 'ui-dialog-mpe',
     minWidth: 300,
     buttons:{
-      'SETTINGS': function(){
+      SETTINGS: function(){
         window.plugin.mpe.dialog.openSettings();
       }
     }
   });
 
   var list = window.plugin.mpe.obj.projects;
-  for(var name in list){
+  for (var name in list){
     window.plugin.mpe.ui.toggleManager(name);
   }
-}
+};
 window.plugin.mpe.dialog.openSettings = function(){
   dialog({
     title: 'Multi Projects Settings',
@@ -292,54 +292,54 @@ window.plugin.mpe.dialog.openSettings = function(){
     dialogClass: 'ui-dialog-mpe',
     minWidth: 300
   });
-}
+};
 window.plugin.mpe.dialog.insertNameNewProject = function(){
   var promptAction = prompt('Choose a Project Name.\nNB: you can use only a-z, A-Z, 0-9, "() []" and " "', '');
 
-  if(promptAction !== null && promptAction !== ''){
+  if (promptAction !== null && promptAction !== ''){
     return promptAction;
   }
   return false;
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // VISIBILITY OPT (IN MANAGER, IN SIDEBAR)
-//------------------------------------------------------
+// ------------------------------------------------------
 window.plugin.mpe.action.toggleSidebar = function(PJ){
   window.plugin.mpe.data.toggleSidebar(PJ);
   window.plugin.mpe.storage.saveStorage();
   window.plugin.mpe.ui.toggleSidebar(PJ);
-}
+};
 window.plugin.mpe.action.toggleManager = function(PJ){
   window.plugin.mpe.data.toggleManager(PJ);
   window.plugin.mpe.storage.saveStorage();
   window.plugin.mpe.ui.toggleManager(PJ);
-}
+};
 
 window.plugin.mpe.data.toggleSidebar = function(PJ){
   var arrSidebar = window.plugin.mpe.obj.opt['settings']['sidebar'];
 
   var index = window.plugin.mpe.data.isInSidebar(PJ);
-  if(index >= 0){
+  if (index >= 0){
     arrSidebar.splice(index, 1);
     return false;
-  }else{
+  } else {
     arrSidebar.push(PJ);
     return true;
   }
-}
+};
 window.plugin.mpe.data.toggleManager = function(PJ){
   var arrSidebar = window.plugin.mpe.obj.opt['settings']['manager'];
 
   var index = window.plugin.mpe.data.isInManager(PJ);
-  if(index >= 0){
+  if (index >= 0){
     arrSidebar.splice(index, 1);
     return false;
-  }else{
+  } else {
     arrSidebar.push(PJ);
     return true;
   }
-}
+};
 
 window.plugin.mpe.ui.toggleSidebar = function (PJ) {
   var entry = $('.mpeSidebar .mpe.' + PJ);
@@ -371,26 +371,26 @@ window.plugin.mpe.ui.toggleManager = function (PJ) {
   var elem = $('.mpeManager .mpe.' + PJ + '');
 
   var isHidden = window.plugin.mpe.data.isInManager(PJ);
-  if(isHidden >= 0){
+  if (isHidden >= 0){
     elem.hide();
-  }else{
+  } else {
     elem.show();
   }
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 // ACTIONS
-//------------------------------------------------------
+// ------------------------------------------------------
 window.plugin.mpe.action.switchProject = function(PJ, storageKey){
   var pj = window.plugin.mpe.obj.projects[PJ];
   var oldKey = pj.currKey;
 
-  //change in obj
+  // change in obj
   window.plugin.mpe.data.setKey(PJ, storageKey);
 
   pj.func_pre();
 
-  //change in plugin
+  // change in plugin
   pj.func_setKey(storageKey);
   console.log('MULTI PROJECTS: switch "'+pj.title+'" storage to "'+storageKey+'".');
 
@@ -407,64 +407,64 @@ window.plugin.mpe.action.switchProject = function(PJ, storageKey){
       prevStorage: oldKey,
       currentStorage: storageKey
     }
-  }
+  };
 
   window.runHooks('mpe', data);
-}
+};
 
 window.plugin.mpe.action.createNewProject = function(PJ, label){
   var name = (label !== undefined)? label : window.plugin.mpe.dialog.insertNameNewProject();
-  if(name === false || name === ''){ return false; }
+  if (name === false || name === ''){ return false; }
 
   var isValid = window.plugin.mpe.data.validateName(name);
-  if(isValid === true){
+  if (isValid === true){
     var storageName = window.plugin.mpe.data.nameToField(PJ, name);
     window.plugin.mpe.data.addStorageToPJ(PJ, storageName);
     //            window.plugin.mpe.ui.appendOption(PJ, newName);
     window.plugin.mpe.action.switchProject(PJ, storageName);
-  }else{
+  } else {
     alert('Failed. Invalid Name');
   }
-}
+};
 window.plugin.mpe.action.deleteProject = function(PJ, storage){
-  if(storage === undefined){
+  if (storage === undefined){
     var storage = window.plugin.mpe.data.getCurrKeyPj(PJ);
   }
 
   var PROJ = window.plugin.mpe.obj.projects[PJ];
   var title = PROJ.title;
   var label = window.plugin.mpe.data.fieldToName(PJ, storage);
-  if(storage === PROJ.defaultKey){ label = 'Default Project'; }
+  if (storage === PROJ.defaultKey){ label = 'Default Project'; }
 
   var promptAction = confirm('The "'+label+'" project data in "'+title+'" will be deleted. Are you sure?', '');
-  if(promptAction){
+  if (promptAction){
     //            window.plugin.mpe.ui.deleteOption(PJ, storage);
     window.plugin.mpe.data.removeStorageFromPJ(PJ, storage);
     window.plugin.mpe.storage.removeStorage(storage);
 
     //            window.plugin.mpe.data.setDefaultKey(PJ);
 
-    //to switch to default_key and load the default storage
+    // to switch to default_key and load the default storage
     var defaultKey = window.plugin.mpe.data.getPreKeyPj(PJ);
     window.plugin.mpe.action.switchProject(PJ, defaultKey);
   }
-}
+};
 
-//------------------------------------------------------
+// ------------------------------------------------------
 
 window.plugin.mpe.setMultiProjects = function(settings){
   window.plugin.mpe.storage.loadStorage();
   window.plugin.mpe.ui.appendContainerInSidebar();
 
-  if(
+  if (
     settings.namespace &&
     settings.defaultKey &&
     settings.func_setKey &&
     settings.func_post
   ){
-    if(!settings.title){ settings.title = 'Untitled'; }
-    if(!settings.fa){ settings.fa = ''; }
-    if(!settings.func_pre){ settings.func_pre = ''; }
+    if (!settings.title){ settings.title = 'Untitled'; }
+    if (!settings.fa){ settings.fa = ''; }
+    if (!settings.func_pre){ settings.func_pre = ''; }
 
     var newMPE = {
       namespace: settings.namespace,
@@ -482,10 +482,10 @@ window.plugin.mpe.setMultiProjects = function(settings){
     window.plugin.mpe.data.scanStorageForOne(settings.namespace);
     window.plugin.mpe.ui.toggleSidebar(settings.namespace);
   }
-}
+};
 
 window.plugin.mpe.setupCSS = function(){
-  $("<style>").prop("type", "text/css").html(''
+  $('<style>').prop('type', 'text/css').html(''
   +'.mpe, .mpe *{text-align:center;box-sizing:border-box;}'
   +'.mpe .left{float:left;}'
   +'.mpe .right{float:right;}'
@@ -510,8 +510,8 @@ window.plugin.mpe.setupCSS = function(){
   +'#sidebar .mpeSidebar .mpe > .fa.nofa:before{content:\'\0\';}'
   +'#sidebar .mpeSidebar .mpe a{}'
   +'#sidebar .mpeSidebar .mpe select{width:63%;}'
-  ).appendTo("head");
-}
+  ).appendTo('head');
+};
 
 
 var setup = function(){
@@ -519,6 +519,6 @@ var setup = function(){
   window.plugin.mpe.setupCSS();
   window.plugin.mpe.ui.addControl();
   window.plugin.mpe.ui.appendContainerInSidebar();
-}
+};
 
 setup.priority = 'high';
