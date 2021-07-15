b7b0d5635323fbb6bc623e3b0c504bbbc70c79fa
johndoe <johnd0e@mail.ua>
2021-07-15 15:30:35 +0300

    Bump versions

--- core/total-conversion-build.js
+++ core/total-conversion-build.js
@@ -1,6 +1,6 @@
 // @author         jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.32.0
+// @version        0.32.1
 // @description    Total conversion for the ingress intel map.
 // @run-at         document-end
 
