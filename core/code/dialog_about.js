MatiasHiltunen/IITC-TypeScript-Edition

Add Warning message if localStorage size is huge

core/code/dialog_about.js
@@ -34,6 +34,10 @@ function createDialogContent() {
     + '<hr>'
     + '<div>Version: ' + getIITCVersion() + '</div>';
 
+  if (isShortOnLocalStorage()) {
+    html += '<div class="warning">You are running low on LocalStorage memory.<br/>Please free some space to prevent data loss.</div>';
+  }
+
   if (typeof android !== 'undefined' && android.getVersionName) {
     html += '<div>IITC Mobile ' + android.getVersionName() + '</div>';
   }
@@ -156,4 +160,18 @@ function formatVerInfo(p, extra) {
   }
 
   return '';
+}
+
+
+function isShortOnLocalStorage() {
+  var MINIMUM_FREE_SPACE = 100000;
+  try {
+    localStorage.setItem('_MEM_CHECK_', '#'.repeat(MINIMUM_FREE_SPACE));
+  } catch (e) {
+    console.error('out of localstorage space', e);
+    return true;
+  }
+
+  localStorage.removeItem('_MEM_CHECK_');
+  return false;
 }

IITC-CE/ingress-intel-total-conversion

core/code/dialog_about.js
@@ -1,4 +1,4 @@
-/* global script_info, app, L */
+/* global script_info, app, log, L */
 
 window.aboutIITC = function() {
   var html = createDialogContent();
@@ -30,6 +30,10 @@ function createDialogContent() {
     + '</div>'
     + '<hr>'
     + '<div>Version: ' + getIITCVersion() + '</div>';
+
+  if (isShortOnLocalStorage()) {
+    html += '<div class="warning">You are running low on LocalStorage memory.<br/>Please free some space to prevent data loss.</div>';
+  }
 
   if (window.isApp && app.getVersionName) {
     html += '<div>IITC Mobile ' + app.getVersionName() + '</div>';
@@ -153,4 +157,18 @@ function formatVerInfo(p, extra) {
   }
 
   return '';
+}
+
+
+function isShortOnLocalStorage() {
+  var MINIMUM_FREE_SPACE = 100000;
+  try {
+    localStorage.setItem('_MEM_CHECK_', '#'.repeat(MINIMUM_FREE_SPACE));
+  } catch (e) {
+    log.error('out of localstorage space', e);
+    return true;
+  }
+
+  localStorage.removeItem('_MEM_CHECK_');
+  return false;
 }