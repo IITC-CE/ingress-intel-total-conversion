96de2d3255e7f87716395068ecb28679396626b5
McBen <viertelvor12@gmx.net>
2022-10-06 17:08:10 +0200

    Load Smartphone.css on mobile

--- core/code/smartphone.js
+++ core/code/smartphone.js
@@ -1,4 +1,4 @@
-window.isSmartphone = function() {
+window.isSmartphone = function () {
   // this check is also used in main.js. Note it should not detect
   // tablets because their display is large enough to use the desktop
   // version.
@@ -12,37 +12,35 @@ window.isSmartphone = function() {
   if (viewParam == 'f') return false;
 
   return navigator.userAgent.match(/Android.*Mobile/)
-  || navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    || navigator.userAgent.match(/iPhone|iPad|iPod/i);
 }
 
-window.smartphone = function() {};
+window.smartphone = function () { };
 
-window.runOnSmartphonesBeforeBoot = function() {
-  if(!isSmartphone()) return;
+window.runOnSmartphonesBeforeBoot = function () {
+  if (!isSmartphone()) return;
   log.warn('running smartphone pre boot stuff');
 
   // add smartphone stylesheet
-  headHTML = document.getElementsByTagName('head')[0].innerHTML;
-  headHTML += `<style>${require("!!raw-loader!../mobile/smartphone.css").default}</style>`;
-  document.getElementsByTagName('head')[0].innerHTML = headHTML;
+  require("../smartphone.css");
 
   // don’t need many of those
-  window.setupStyles = function() {
+  window.setupStyles = function () {
     $('head').append('<style>' +
-      [ '#largepreview.enl img { border:2px solid '+COLORS[TEAM_ENL]+'; } ',
-        '#largepreview.res img { border:2px solid '+COLORS[TEAM_RES]+'; } ',
-        '#largepreview.none img { border:2px solid '+COLORS[TEAM_NONE]+'; } '].join("\n")
+      ['#largepreview.enl img { border:2px solid ' + COLORS[TEAM_ENL] + '; } ',
+      '#largepreview.res img { border:2px solid ' + COLORS[TEAM_RES] + '; } ',
+      '#largepreview.none img { border:2px solid ' + COLORS[TEAM_NONE] + '; } '].join("\n")
       + '</style>');
   }
 
-  window.smartphone.mapButton = $('<a>map</a>').click(function() {
-    $('#map').css({'visibility': 'visible', 'opacity': '1'});
+  window.smartphone.mapButton = $('<a>map</a>').click(function () {
+    $('#map').css({ 'visibility': 'visible', 'opacity': '1' });
     $('#updatestatus').show();
     $('#chatcontrols a .active').removeClass('active');
     $("#chatcontrols a:contains('map')").addClass('active');
   });
 
-  window.smartphone.sideButton = $('<a>info</a>').click(function() {
+  window.smartphone.sideButton = $('<a>info</a>').click(function () {
     $('#scrollwrapper').show();
     window.resetScrollOnNewPortal();
     $('.active').removeClass('active');
@@ -51,15 +49,15 @@ window.runOnSmartphonesBeforeBoot = function() {
 
   $('#chatcontrols').append(smartphone.mapButton).append(smartphone.sideButton);
 
-  window.addHook('portalDetailsUpdated', function(data) {
+  window.addHook('portalDetailsUpdated', function (data) {
     var x = $('.imgpreview img').removeClass('hide');
 
-    if(!x.length) {
+    if (!x.length) {
       $('.fullimg').remove();
       return;
     }
 
-    if($('.fullimg').length) {
+    if ($('.fullimg').length) {
       $('.fullimg').replaceWith(x.addClass('fullimg'));
     } else {
       x.addClass('fullimg').appendTo('#sidebar');
@@ -67,37 +65,36 @@ window.runOnSmartphonesBeforeBoot = function() {
   });
 }
 
-window.smartphoneInfo = function(data) {
+window.smartphoneInfo = function (data) {
   var guid = data.selectedPortalGuid;
-  if(!window.portals[guid]) return;
+  if (!window.portals[guid]) return;
 
   var data = window.portals[selectedPortal].options.data;
-  if(typeof data.title === 'undefined') return;
+  if (typeof data.title === 'undefined') return;
 
   var details = window.portalDetail.get(guid);
 
   var lvl = data.level;
-  if(data.team === "N" || data.team === "NEUTRAL")
+  if (data.team === "N" || data.team === "NEUTRAL")
     var t = '<span class="portallevel">L0</span>';
   else
-    var t = '<span class="portallevel" style="background: '+COLORS_LVL[lvl]+';">L' + lvl + '</span>';
+    var t = '<span class="portallevel" style="background: ' + COLORS_LVL[lvl] + ';">L' + lvl + '</span>';
 
   var percentage = data.health;
-  if(details) {
+  if (details) {
     var totalEnergy = getTotalPortalEnergy(details);
-    if(getTotalPortalEnergy(details) > 0) {
+    if (getTotalPortalEnergy(details) > 0) {
       percentage = Math.floor(getCurrentPortalEnergy(details) / totalEnergy * 100);
     }
   }
   t += ' ' + percentage + '% ';
   t += data.title;
 
-  if(details) {
-    var l,v,max,perc;
-    var eastAnticlockwiseToNorthClockwise = [2,1,0,7,6,5,4,3];
+  if (details) {
+    var l, v, max, perc;
+    var eastAnticlockwiseToNorthClockwise = [2, 1, 0, 7, 6, 5, 4, 3];
 
-    for(var ind=0;ind<8;ind++)
-    {
+    for (var ind = 0; ind < 8; ind++) {
       if (details.resonators.length == 8) {
         var slot = eastAnticlockwiseToNorthClockwise[ind];
         var reso = details.resonators[slot];
@@ -107,13 +104,13 @@ window.smartphoneInfo = function(data) {
       }
 
       var className = TEAM_TO_CSS[getTeam(details)];
-      if(slot !== null && OCTANTS[slot] === 'N')
+      if (slot !== null && OCTANTS[slot] === 'N')
         className += ' north'
-      if(reso) {
+      if (reso) {
         l = parseInt(reso.level);
         v = parseInt(reso.energy);
         max = RESO_NRG[l];
-        perc = v/max*100;
+        perc = v / max * 100;
       } else {
         l = 0;
         v = 0;
@@ -121,8 +118,8 @@ window.smartphoneInfo = function(data) {
         perc = 0;
       }
 
-      t += '<div class="resonator '+className+'" style="border-top-color: '+COLORS_LVL[l]+';left: '+(100*ind/8.0)+'%;">';
-      t += '<div class="filllevel" style="width:'+perc+'%;"></div>';
+      t += '<div class="resonator ' + className + '" style="border-top-color: ' + COLORS_LVL[l] + ';left: ' + (100 * ind / 8.0) + '%;">';
+      t += '<div class="filllevel" style="width:' + perc + '%;"></div>';
       t += '</div>'
     }
   }
@@ -130,8 +127,8 @@ window.smartphoneInfo = function(data) {
   $('#mobileinfo').html(t);
 }
 
-window.runOnSmartphonesAfterBoot = function() {
-  if(!isSmartphone()) return;
+window.runOnSmartphonesAfterBoot = function () {
+  if (!isSmartphone()) return;
   log.warn('running smartphone post boot stuff');
 
   window.show('map');
@@ -145,7 +142,7 @@ window.runOnSmartphonesAfterBoot = function() {
   // replace img full view handler
   $('#portaldetails')
     .off('click', '.imgpreview')
-    .on('click', '.imgpreview', function(e) {
+    .on('click', '.imgpreview', function (e) {
       if (e.currentTarget === e.target) { // do not fire on #level
         $('.ui-tooltip').remove();
         var newTop = $('.fullimg').position().top + $("#sidebar").scrollTop();
@@ -155,5 +152,5 @@ window.runOnSmartphonesAfterBoot = function() {
 
   // make buttons in action bar flexible
   var l = $('#chatcontrols a:visible');
-  l.css('width', 100/l.length + '%');
+  l.css('width', 100 / l.length + '%');
 }
