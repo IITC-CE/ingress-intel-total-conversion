a8e5ee9de5e00b16a7b32a14eac13bb716113c5b
McBen <viertelvor12@gmx.net>
2022-11-02 14:06:18 +0100

    Revert "Remove outdated Leaflet option"
    Still supported option. Missing in Leaflet docs.
    
    This reverts commit 289e76bb89f585ada310cebfde52b36b07b09627.

--- core/code/map.js
+++ core/code/map.js
@@ -287,7 +287,7 @@ window.setupMap = function () {
       pos = {center: [0, 0], zoom: 1};
       map.locate({setView: true});
     }
-    map.setView(pos.center, pos.zoom);
+    map.setView(pos.center, pos.zoom, {reset: true});
 
     // read here ONCE, so the URL is only evaluated one time after the
     // necessary data has been loaded.
