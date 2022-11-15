2a064df3c7e414cb9b3a069dbff291708aa3e725
Cestmir Ruzicka <cestmir.ruzicka@gmail.com>
2022-11-15 01:26:26 +0100

    clean entity_info.js

--- core/code/entity_info.js
+++ core/code/entity_info.js
@@ -1,26 +1,30 @@
 /* exported setup --eslint */
-/* global TEAM_NONE, TEAM_RES, TEAM_ENL, TEAM_MAC */
 
 // ENTITY DETAILS TOOLS //////////////////////////////////////////////
 // hand any of these functions the details-hash of an entity (i.e.
 // portal, link, field) and they will return useful data.
 
-
 // given the entity detail data, returns the team the entity belongs
 // to. Uses TEAM_* enum values.
-window.getTeam = function(details) {
-  return teamStringToId(details.team);
-}
+window.getTeam = function (details) {
+  return window.teamStringToId(details.team);
+};
 
-window.teamStringToId = function(teamStr) {
-  var team = TEAM_NONE;
-  if(teamStr === 'ENLIGHTENED') team = TEAM_ENL;
-  if(teamStr === 'RESISTANCE') team = TEAM_RES;
-  if (teamStr === window.TEAM_NAME_MAC) team = TEAM_MAC;
-  if(teamStr === 'E') team = TEAM_ENL;
-  if(teamStr === 'R') team = TEAM_RES;
-  if (teamStr === window.TEAM_CODE_MAC) team = TEAM_MAC;
-  return team;
-}
+window.teamStringToId = function (teamStr) {
+  switch (teamStr) {
+    case window.TEAM_NAME_ENL:
+    case window.TEAM_CODE_ENL:
+      return window.TEAM_ENL;
 
+    case window.TEAM_NAME_RES:
+    case window.TEAM_CODE_RES:
+      return window.TEAM_RES;
 
+    case window.TEAM_NAME_MAC:
+    case window.TEAM_CODE_MAC:
+      return window.TEAM_MAC;
+
+    default:
+      return window.TEAM_NONE;
+  }
+};
