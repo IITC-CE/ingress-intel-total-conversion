8bd0796ece43abbca8baf895c51a58cf4466695e
perringaiden <brent.hollett@gmail.com>
2022-11-19 16:30:02 +1000

    Added FindParent and FindSeed functions.

--- iitc-plugin-wolf-machina.user.js
+++ iitc-plugin-wolf-machina.user.js
@@ -2,7 +2,7 @@
 // @author         Perringaiden
 // @name           IITC plugin: Machina Tools
 // @category       Misc
-// @version        0.01
+// @version        0.1
 // @description    Machina investigation tools
 // @id             misc-wolf-machina
 // @updateURL      https://bitbucket.org/perringaiden/iitc/raw/master/iitc-plugin-wolf-machina.meta.js
@@ -13,28 +13,98 @@
 
 function wrapper(plugin_info) {
     // ensure plugin framework is there, even if iitc is not yet loaded
-    if (typeof window.plugin !== 'function') window.plugin = function() {};
+    if (typeof window.plugin !== 'function') window.plugin = function () { };
 
     // use own namespace for plugin
-    window.plugin.wolfMachina = function() {};
+    window.plugin.wolfMachina = function () { };
 
-    window.plugin.wolfMachina.findParent = function(portalGuid) {}
+    window.plugin.wolfMachina.findParent = function (portalGuid) {
+        // Get the portal's data.
+        let parent = undefined;
+
+        debugger;
+
+        if (portalGuid !== 'undefined') {
+
+            let linkGuids = getPortalLinks(portalGuid);
+            $.each(linkGuids.in.concat(linkGuids.out), function (i, lguid) {
+                let l = window.links[lguid];
+                let ld = l.options.data;
+
+                if (ld.dGuid == portalGuid) {
+                    parent = {};
+                    parent.guid = ld.oGuid;
+                    parent.lat = ld.oLatE6 / 1E6;
+                    parent.lng = ld.oLngE6 / 1E6;
+
+                    return false;
+                }
+            });
+
+        }
+
+        return parent;
+    };
+
+    window.plugin.wolfMachina.goToParent = function (portalGuid) {
+        let parent;
 
 
+        parent = window.plugin.wolfMachina.findParent(portalGuid);
 
-    window.plugin.wolfMachina.onPortalDetailsUpdated = function() {
+        if (parent != undefined) {
+            window.zoomToAndShowPortal(parent.guid, [parent.lat, parent.lng]);
+        } else {
+            dialog({
+                html: $('<div id="no-machina-parent">No Parent found.</div>'),
+                title: 'Machina Tools',
+                id: 'no-machina-parent'
+            });
+        }
+    };
+
+    window.plugin.wolfMachina.goToSeed = function (portalGuid) {
+        let parent;
+
+        while (portalGuid != undefined) {
+            let newParent;
+
+            newParent = window.plugin.wolfMachina.findParent(portalGuid);
+
+            if (newParent != undefined) {
+                parent = newParent;
+                portalGuid = newParent.guid;
+            } else {
+                portalGuid = undefined;
+            }
+        }
+
+        if (parent !== undefined) {
+            window.zoomToAndShowPortal(parent.guid, [parent.lat, parent.lng]);
+        }
+    }
+
+    window.plugin.wolfMachina.onPortalDetailsUpdated = function () {
+        let portalData;
+
         // If the portal was cleared then exit.
         if (window.selectedPortal === null) return;
 
-        // Add the 'find Parent' button.
-        $('.linkdetails').append('<a onclick="window.plugin.wolfMachina.findParent("' + window.selectedPortal + '")" title=" Find Machina Parent ">Find Parent</a>');
+        portalData = portalDetail.get(window.selectedPortal);
 
-        // Add the 'trace children' button.
+        debugger;
+        if (portalData.team == "M") {
 
+            // Add the 'find Parent' button.
+            $('.linkdetails').append('<aside><a onclick="window.plugin.wolfMachina.goToParent(\'' + window.selectedPortal + '\')" title=" Find Machina Parent ">Find Parent</a></aside>');
+            $('.linkdetails').append('<aside><a onclick="window.plugin.wolfMachina.goToSeed(\'' + window.selectedPortal + '\')" title="Find Machina Seed">Find Seed</a></aside>');
+            // Add the 'trace children' button.
 
+        }
     };
 
-    var setup = function() {
+
+    let setup = function () {
         window.addHook('portalDetailsUpdated', window.plugin.wolfMachina.onPortalDetailsUpdated);
     }
 
@@ -45,8 +115,8 @@ function wrapper(plugin_info) {
     if (window.iitcLoaded && typeof setup === 'function') setup();
 } // wrapper end
 // inject code into site context
-var script = document.createElement('script');
-var info = {};
+let script = document.createElement('script');
+let info = {};
 if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = {
     version: GM_info.script.version,
     name: GM_info.script.name,
