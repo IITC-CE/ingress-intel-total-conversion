7113b70785faf2fe325a3bf009722a518240899d
MysticJay <gtp7501mj@gmail.com>
2022-12-09 18:27:30 +0100

    eslint fixes
    Eslint fixes

--- core/code/portal_info.js
+++ core/code/portal_info.js
@@ -56,7 +56,7 @@ window.getPortalRange = function(d) {
   });
 
   var range = {
-    base: d.team === 'M' ? window.LINK_RANGE_MAC[d.level] : 160*Math.pow(getPortalLevel(d), 4),
+    base: d.team === 'M' ? window.LINK_RANGE_MAC[d.level] : 160 * Math.pow(window.getPortalLevel(d), 4),
     boost: getLinkAmpRangeBoost(d)
   };
 
