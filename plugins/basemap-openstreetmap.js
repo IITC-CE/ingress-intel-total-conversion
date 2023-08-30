bd40fac38f3b6d2b37d81b70cf795a6b036c7614
LeJeu <64744459+le-jeu@users.noreply.github.com>
2023-08-30 12:11:38 +0200

    osm: expose config
    allow early boot plugins to changes layer names/url

--- plugins/basemap-openstreetmap.js
+++ plugins/basemap-openstreetmap.js
@@ -9,36 +9,48 @@
 
 // use own namespace for plugin
 var mapOpenStreetMap = {};
+window.plugin.mapOpenStreetMap = mapOpenStreetMap;
 
 var changelog = [
   {
     version: '0.1.3',
-    changes: ['Update OSM tile provider', 'Add CyclOSM tiles'],
+    changes: ['Update OSM tile provider', 'Add CyclOSM tiles', 'Expose config'],
   },
 ];
 
-mapOpenStreetMap.addLayer = function () {
+// https://wiki.openstreetmap.org/wiki/Raster_tile_providers
+
+// Common options
+var osmOpt = {
+  attribution: 'Map data © OpenStreetMap contributors',
+  maxNativeZoom: 18,
+  maxZoom: 21,
+};
+
+mapOpenStreetMap.LAYERS = [
+  {
+    name: 'OpenStreetMap',
+    url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    options: Object.assign({}, osmOpt),
+  },
+  {
+    name: 'Humanitarian',
+    url: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+    options: Object.assign({}, osmOpt),
+  },
+  {
+    name: 'CyclOSM',
+    url: 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+    options: Object.assign({}, osmOpt),
+  },
+];
+
+function setup() {
   // OpenStreetMap tiles - we shouldn't use these by default - https://wiki.openstreetmap.org/wiki/Tile_usage_policy
   // "Heavy use (e.g. distributing an app that uses tiles from openstreetmap.org) is forbidden without prior permission from the System Administrators"
 
-  var osmOpt = {
-    attribution: 'Map data © OpenStreetMap contributors',
-    maxNativeZoom: 18,
-    maxZoom: 21,
-  };
-
-  var layers = {
-    'https://tile.openstreetmap.org/{z}/{x}/{y}.png': 'OpenStreetMap',
-    'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png': 'Humanitarian',
-    'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png': 'CyclOSM',
-  };
-
-  for (var url in layers) {
-    var layer = new L.TileLayer(url, osmOpt);
-    layerChooser.addBaseLayer(layer, layers[url]);
+  for (var entry of mapOpenStreetMap.LAYERS) {
+    var layer = new L.TileLayer(entry.url, entry.options);
+    layerChooser.addBaseLayer(layer, entry.name);
   }
-};
-
-function setup() {
-  mapOpenStreetMap.addLayer();
 }
