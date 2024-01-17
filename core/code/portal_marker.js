a649834200684a3ad27b7c5fd096ce9b02a4b563
Alexander Danilov <git@modos189.ru>
2024-01-17 13:52:51 +0600

    JSDoc for core/portal_marker.js

--- core/code/portal_marker.js
+++ core/code/portal_marker.js
@@ -1,7 +1,14 @@
-// PORTAL MARKER //////////////////////////////////////////////
-// code to create and update a portal marker
+/**
+ * @file This file contains the code related to creating and updating portal markers on the map.
+ * @module portal_marker
+ */
 
-
+/**
+ * Calculates the scale of portal markers based on the current zoom level of the map.
+ *
+ * @function portalMarkerScale
+ * @returns {number} The scale factor for portal markers.
+ */
 window.portalMarkerScale = function() {
   var zoom = map.getZoom();
   if (L.Browser.mobile)
@@ -10,7 +17,14 @@ window.portalMarkerScale = function() {
     return zoom >= 14 ? 1 : zoom >= 11 ? 0.8 : zoom >= 8 ? 0.65 : 0.5;
 }
 
-// create a new marker. 'data' contain the IITC-specific entity data to be stored in the object options
+/**
+ * Creates a new portal marker on the map.
+ *
+ * @function createMarker
+ * @param {L.LatLng} latlng - The latitude and longitude where the marker will be placed.
+ * @param {Object} data - The IITC-specific entity data to be stored in the marker options.
+ * @returns {L.circleMarker} A Leaflet circle marker representing the portal.
+ */
 window.createMarker = function(latlng, data) {
   var styleOptions = window.getMarkerStyleOptions(data);
 
@@ -23,7 +37,13 @@ window.createMarker = function(latlng, data) {
   return marker;
 }
 
-
+/**
+ * Sets the style of a portal marker, including options for when the portal is selected.
+ *
+ * @function setMarkerStyle
+ * @param {L.circleMarker} marker - The portal marker whose style will be set.
+ * @param {boolean} selected - Indicates if the portal is selected.
+ */
 window.setMarkerStyle = function(marker, selected) {
 
   var styleOptions = window.getMarkerStyleOptions(marker.options);
@@ -39,7 +59,13 @@ window.setMarkerStyle = function(marker, selected) {
   }
 }
 
-
+/**
+ * Determines the style options for a portal marker based on its details.
+ *
+ * @function getMarkerStyleOptions
+ * @param {Object} details - Details of the portal, including team and level.
+ * @returns {Object} Style options for the portal marker.
+ */
 window.getMarkerStyleOptions = function(details) {
   var scale = window.portalMarkerScale();
 
