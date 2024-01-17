75b9e58886a9f1e2720df0fa650c0d97031c9b3d
Alexander Danilov <git@modos189.ru>
2024-01-17 13:52:34 +0600

    JSDoc for core/portal_highlighter.js

--- core/code/portal_highlighter.js
+++ core/code/portal_highlighter.js
@@ -1,5 +1,7 @@
-// Portal Highlighter //////////////////////////////////////////////////////////
-// these functions handle portal highlighters
+/**
+ * @file These functions handle portal highlighters
+ * @module portal_highlighter
+ */
 
 // an object mapping highlighter names to the object containing callback functions
 window._highlighters = null;
@@ -9,7 +11,14 @@ window._current_highlighter = localStorage.portal_highlighter;
 
 window._no_highlighter = 'No Highlights';
 
-
+/**
+ * Adds a new portal highlighter to map. The highlighter is a function that will be called for each portal.
+ *
+ * @function addPortalHighlighter
+ * @param {string} name - The name of the highlighter.
+ * @param {Function} data - The callback function for the highlighter.
+ *                          This function receives data about the portal and decides how to highlight it.
+ */
 window.addPortalHighlighter = function(name, data) {
   if(_highlighters === null) {
     _highlighters = {};
@@ -33,7 +42,7 @@ window.addPortalHighlighter = function(name, data) {
     if (window.isApp && app.setActiveHighlighter)
       app.setActiveHighlighter(name);
 
-    // call the setSelected callback 
+    // call the setSelected callback
     if (_highlighters[_current_highlighter].setSelected) {
       _highlighters[_current_highlighter].setSelected(true);
     }
@@ -42,7 +51,11 @@ window.addPortalHighlighter = function(name, data) {
   updatePortalHighlighterControl();
 }
 
-// (re)creates the highlighter dropdown list
+/**
+ * Updates the portal highlighter dropdown list, recreating the dropdown list of available highlighters.
+ *
+ * @function updatePortalHighlighterControl
+ */
 window.updatePortalHighlighterControl = function() {
   if (isApp && app.addPortalHighlighter) {
     $('#portal_highlight_select').remove();
@@ -59,8 +72,8 @@ window.updatePortalHighlighterControl = function() {
     $("#portal_highlight_select").html('');
     $("#portal_highlight_select").append($("<option>").attr('value',_no_highlighter).text(_no_highlighter));
     var h_names = Object.keys(_highlighters).sort();
-    
-    $.each(h_names, function(i, name) {  
+
+    $.each(h_names, function(i, name) {
       $("#portal_highlight_select").append($("<option>").attr('value',name).text(name));
     });
 
@@ -68,6 +81,12 @@ window.updatePortalHighlighterControl = function() {
   }
 }
 
+/**
+ * Changes the current portal highlights based on the selected highlighter.
+ *
+ * @function changePortalHighlights
+ * @param {string} name - The name of the highlighter to be applied.
+ */
 window.changePortalHighlights = function(name) {
 
   // first call any previous highlighter select callback
@@ -88,13 +107,25 @@ window.changePortalHighlights = function(name) {
   localStorage.portal_highlighter = name;
 }
 
+/**
+ * Applies the currently active highlighter to a specific portal.
+ * This function is typically called for each portal on the map.
+ *
+ * @function highlightPortal
+ * @param {Object} p - The portal object to be highlighted.
+ */
 window.highlightPortal = function(p) {
-  
+
   if(_highlighters !== null && _highlighters[_current_highlighter] !== undefined) {
     _highlighters[_current_highlighter].highlight({portal: p});
   }
 }
 
+/**
+ * Resets the highlighting of all portals, returning them to their default style.
+ *
+ * @function resetHighlightedPortals
+ */
 window.resetHighlightedPortals = function() {
   $.each(portals, function(guid, portal) {
     setMarkerStyle(portal, guid === selectedPortal);
