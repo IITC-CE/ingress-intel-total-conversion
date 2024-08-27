5ede5c9c3b07a48096a25c965feae867f28ef76b
Alexander Danilov <git@modos189.ru>
2024-08-27 21:30:03 +0500

    Fix eslint for core/code/map_data_calc_tools.js +remove `debugger` keywords from code +remove erroneous tileParams value setting when returning window.getDataZoomTileParameters

--- core/code/map_data_calc_tools.js
+++ core/code/map_data_calc_tools.js
@@ -1,3 +1,5 @@
+/* global log -- eslint */
+
 /**
  * @file Contains functions for calculating map data request parameters and converting between lat/lng and map tiles.
  * Ingress Intel splits up requests for map data (portals, links, fields) into tiles.
@@ -15,41 +17,37 @@
  *
  * @function setupDataTileParams
  */
-window.setupDataTileParams = function() {
+window.setupDataTileParams = function () {
   // default values - used to fall back to if we can't detect those used in stock intel
-  var DEFAULT_ZOOM_TO_TILES_PER_EDGE = [1,1,1,40,40,80,80,320,1000,2000,2000,4000,8000,16000,16000,32000];
-  var DEFAULT_ZOOM_TO_LEVEL = [8,8,8,8,7,7,7,6,6,5,4,4,3,2,2,1,1];
+  var DEFAULT_ZOOM_TO_TILES_PER_EDGE = [1, 1, 1, 40, 40, 80, 80, 320, 1000, 2000, 2000, 4000, 8000, 16000, 16000, 32000];
+  var DEFAULT_ZOOM_TO_LEVEL = [8, 8, 8, 8, 7, 7, 7, 6, 6, 5, 4, 4, 3, 2, 2, 1, 1];
 
   // stock intel doesn't have this array (they use a switch statement instead), but this is far neater
-  var DEFAULT_ZOOM_TO_LINK_LENGTH = [200000,200000,200000,200000,200000,60000,60000,10000,5000,2500,2500,800,300,0,0];
+  var DEFAULT_ZOOM_TO_LINK_LENGTH = [200000, 200000, 200000, 200000, 200000, 60000, 60000, 10000, 5000, 2500, 2500, 800, 300, 0, 0];
 
   window.TILE_PARAMS = {};
 
   // not in stock to detect - we'll have to assume the above values...
   window.TILE_PARAMS.ZOOM_TO_LINK_LENGTH = DEFAULT_ZOOM_TO_LINK_LENGTH;
 
-
-  if (niantic_params.ZOOM_TO_LEVEL && niantic_params.TILES_PER_EDGE) {
-    window.TILE_PARAMS.ZOOM_TO_LEVEL = niantic_params.ZOOM_TO_LEVEL;
-    window.TILE_PARAMS.TILES_PER_EDGE = niantic_params.TILES_PER_EDGE;
-
+  if (window.niantic_params.ZOOM_TO_LEVEL && window.niantic_params.TILES_PER_EDGE) {
+    window.TILE_PARAMS.ZOOM_TO_LEVEL = window.niantic_params.ZOOM_TO_LEVEL;
+    window.TILE_PARAMS.TILES_PER_EDGE = window.niantic_params.TILES_PER_EDGE;
 
     // lazy numerical array comparison
-    if (JSON.stringify(niantic_params.ZOOM_TO_LEVEL) !== JSON.stringify(DEFAULT_ZOOM_TO_LEVEL)) {
+    if (JSON.stringify(window.niantic_params.ZOOM_TO_LEVEL) !== JSON.stringify(DEFAULT_ZOOM_TO_LEVEL)) {
       log.warn('Tile parameter ZOOM_TO_LEVEL have changed in stock intel. Detected correct values, but code should be updated');
-      debugger;
     }
-    if (JSON.stringify(niantic_params.TILES_PER_EDGE) !== JSON.stringify(DEFAULT_ZOOM_TO_TILES_PER_EDGE)) {
+    if (JSON.stringify(window.niantic_params.TILES_PER_EDGE) !== JSON.stringify(DEFAULT_ZOOM_TO_TILES_PER_EDGE)) {
       log.warn('Tile parameter TILES_PER_EDGE have changed in stock intel. Detected correct values, but code should be updated');
-      debugger;
     }
-
   } else {
-    dialog({
+    window.dialog({
       title: 'IITC Warning',
-      html: "<p>IITC failed to detect the ZOOM_TO_LEVEL and/or TILES_PER_EDGE settings from the stock intel site.</p>"
-           +"<p>IITC is now using fallback default values. However, if detection has failed it's likely the values have changed."
-           +" IITC may not load the map if these default values are wrong.</p>",
+      html:
+        '<p>IITC failed to detect the ZOOM_TO_LEVEL and/or TILES_PER_EDGE settings from the stock intel site.</p>' +
+        "<p>IITC is now using fallback default values. However, if detection has failed it's likely the values have changed." +
+        ' IITC may not load the map if these default values are wrong.</p>',
     });
 
     window.TILE_PARAMS.ZOOM_TO_LEVEL = DEFAULT_ZOOM_TO_LEVEL;
@@ -59,9 +57,8 @@ window.setupDataTileParams = function() {
   // 2015-07-01: niantic added code to the stock site that overrides the min zoom level for unclaimed portals to 15 and above
   // instead of updating the zoom-to-level array. makes no sense really....
   // we'll just chop off the array at that point, so the code defaults to level 0 (unclaimed) everywhere...
-  window.TILE_PARAMS.ZOOM_TO_LEVEL = window.TILE_PARAMS.ZOOM_TO_LEVEL.slice(0,15); // deprecated
-
-}
+  window.TILE_PARAMS.ZOOM_TO_LEVEL = window.TILE_PARAMS.ZOOM_TO_LEVEL.slice(0, 15); // deprecated
+};
 
 /**
  * Gets the map zoom tile parameters for a specific zoom level. It calculates the tile level, number of tiles per edge,
@@ -78,16 +75,16 @@ window.getMapZoomTileParameters = function (zoom) {
     level: window.TILE_PARAMS.ZOOM_TO_LEVEL[zoom] || 0, // deprecated
     tilesPerEdge: window.TILE_PARAMS.TILES_PER_EDGE[zoom] || maxTilesPerEdge,
     minLinkLength: window.TILE_PARAMS.ZOOM_TO_LINK_LENGTH[zoom] || 0,
-    hasPortals: zoom >= window.TILE_PARAMS.ZOOM_TO_LINK_LENGTH.length,  // no portals returned at all when link length limits things
-    zoom: zoom  // include the zoom level, for reference
+    hasPortals: zoom >= window.TILE_PARAMS.ZOOM_TO_LINK_LENGTH.length, // no portals returned at all when link length limits things
+    zoom: zoom, // include the zoom level, for reference
   };
-}
+};
 
-window.getDataZoomTileParameters = function(zoom) {
-  zoom = arguments.length ? zoom : map.getZoom();
-  var dataZoom = getDataZoomForMapZoom(zoom);
-  return tileParams = getMapZoomTileParameters(dataZoom);
-}
+window.getDataZoomTileParameters = function (zoom) {
+  zoom = arguments.length ? zoom : window.map.getZoom();
+  var dataZoom = window.getDataZoomForMapZoom(zoom);
+  return window.getMapZoomTileParameters(dataZoom);
+};
 
 /**
  * Determines the data zoom level for a given map zoom level. This function adjusts the zoom level for
@@ -97,10 +94,10 @@ window.getDataZoomTileParameters = function(zoom) {
  * @param {number} zoom - The current map zoom level.
  * @returns {number} The adjusted zoom level for data requests.
  */
-window.getDataZoomForMapZoom = function(zoom) {
+window.getDataZoomForMapZoom = function (zoom) {
   // we can fetch data at a zoom level different to the map zoom.
 
-  //NOTE: the specifics of this are tightly coupled with the above ZOOM_TO_LEVEL and TILES_PER_EDGE arrays
+  // NOTE: the specifics of this are tightly coupled with the above ZOOM_TO_LEVEL and TILES_PER_EDGE arrays
 
   // firstly, some of IITCs zoom levels, depending on base map layer, can be higher than stock. limit zoom level
   // (stock site max zoom may vary depending on google maps detail in the area - 20 or 21 max is common)
@@ -112,48 +109,48 @@ window.getDataZoomForMapZoom = function(zoom) {
   // to avoid impacting server load, we keep ourselves restricted to a zoom level with the sane number
   // of tilesPerEdge and portal levels visible
 
-  var origTileParams = getMapZoomTileParameters(zoom);
+  var origTileParams = window.getMapZoomTileParameters(zoom);
 
-  while (zoom > MIN_ZOOM) {
-    var newTileParams = getMapZoomTileParameters(zoom-1);
+  while (zoom > window.MIN_ZOOM) {
+    var newTileParams = window.getMapZoomTileParameters(zoom - 1);
 
-    if ( newTileParams.tilesPerEdge != origTileParams.tilesPerEdge
-      || newTileParams.hasPortals != origTileParams.hasPortals
-      || newTileParams.level*newTileParams.hasPortals != origTileParams.level*origTileParams.hasPortals  // multiply by 'hasPortals' bool - so comparison does not matter when no portals available
+    if (
+      newTileParams.tilesPerEdge !== origTileParams.tilesPerEdge ||
+      newTileParams.hasPortals !== origTileParams.hasPortals ||
+      newTileParams.level * newTileParams.hasPortals !== origTileParams.level * origTileParams.hasPortals // multiply by 'hasPortals' bool - so comparison does not matter when no portals available
     ) {
       // switching to zoom-1 would result in a different detail level - so we abort changing things
       break;
     } else {
       // changing to zoom = zoom-1 results in identical tile parameters - so we can safely step back
       // with no increase in either server load or number of requests
-      zoom = zoom-1;
+      zoom = zoom - 1;
     }
   }
 
   return zoom;
-}
+};
 
-window.lngToTile = function(lng, params) {
-  return Math.floor((lng + 180) / 360 * params.tilesPerEdge);
-}
+window.lngToTile = function (lng, params) {
+  return Math.floor(((lng + 180) / 360) * params.tilesPerEdge);
+};
 
-window.latToTile = function(lat, params) {
-  return Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) +
-    1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * params.tilesPerEdge);
-}
+window.latToTile = function (lat, params) {
+  return Math.floor(((1 - Math.log(Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)) / Math.PI) / 2) * params.tilesPerEdge);
+};
 
-window.tileToLng = function(x, params) {
-  return x / params.tilesPerEdge * 360 - 180;
-}
+window.tileToLng = function (x, params) {
+  return (x / params.tilesPerEdge) * 360 - 180;
+};
 
-window.tileToLat = function(y, params) {
-  var n = Math.PI - 2 * Math.PI * y / params.tilesPerEdge;
-  return 180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
-}
+window.tileToLat = function (y, params) {
+  var n = Math.PI - (2 * Math.PI * y) / params.tilesPerEdge;
+  return (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
+};
 
-window.pointToTileId = function(params, x, y) {
-//change to quadkey construction
-//as of 2014-05-06: zoom_x_y_minlvl_maxlvl_maxhealth
+window.pointToTileId = function (params, x, y) {
+  // change to quadkey construction
+  // as of 2014-05-06: zoom_x_y_minlvl_maxlvl_maxhealth
 
-  return params.zoom + "_" + x + "_" + y + "_" + params.level + "_8_100";
-}
+  return params.zoom + '_' + x + '_' + y + '_' + params.level + '_8_100';
+};
