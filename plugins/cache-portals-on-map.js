5426088fb6cf08f84909b31bcbf53b59075daf31
Alexander Danilov <git@modos189.ru>
2024-09-28 22:26:46 +0500

    Fix eslint for plugins/cache-portals-on-map.js

--- plugins/cache-portals-on-map.js
+++ plugins/cache-portals-on-map.js
@@ -1,12 +1,16 @@
 // @author         jonatkins
 // @name           Cache viewed portals on map
 // @category       Cache
-// @version        0.1.1
+// @version        0.1.2
 // @description    Cache the details of recently viewed portals and use this to populate the map when possible
 
 /* exported setup, changelog --eslint */
 
 var changelog = [
+  {
+    version: '0.1.2',
+    changes: ['Refactoring: fix eslint'],
+  },
   {
     version: '0.1.1',
     changes: ['Version upgrade due to a change in the wrapper: added plugin icon'],
@@ -14,18 +18,18 @@ var changelog = [
 ];
 
 // use own namespace for plugin
-window.plugin.cachePortalDetailsOnMap = function() {};
+window.plugin.cachePortalDetailsOnMap = function () {};
 
-window.plugin.cachePortalDetailsOnMap.MAX_AGE = 12*60*60;  //12 hours max age for cached data
+window.plugin.cachePortalDetailsOnMap.MAX_AGE = 12 * 60 * 60; // 12 hours max age for cached data
 
-window.plugin.cachePortalDetailsOnMap.portalDetailLoaded = function(data) {
+window.plugin.cachePortalDetailsOnMap.portalDetailLoaded = function (data) {
   if (data.success) {
     window.plugin.cachePortalDetailsOnMap.cache[data.guid] = { loadtime: Date.now(), ent: data.ent };
   }
 };
 
-window.plugin.cachePortalDetailsOnMap.entityInject = function(data) {
-  var maxAge = Date.now() - window.plugin.cachePortalDetailsOnMap.MAX_AGE*1000;
+window.plugin.cachePortalDetailsOnMap.entityInject = function (data) {
+  var maxAge = Date.now() - window.plugin.cachePortalDetailsOnMap.MAX_AGE * 1000;
 
   var ents = [];
   for (var guid in window.plugin.cachePortalDetailsOnMap.cache) {
@@ -38,13 +42,11 @@ window.plugin.cachePortalDetailsOnMap.entityInject = function(data) {
   data.callback(ents, 'detailed');
 };
 
-
-window.plugin.cachePortalDetailsOnMap.setup  = function() {
-
+window.plugin.cachePortalDetailsOnMap.setup = function () {
   window.plugin.cachePortalDetailsOnMap.cache = {};
 
-  addHook('portalDetailLoaded', window.plugin.cachePortalDetailsOnMap.portalDetailLoaded);
-  addHook('mapDataEntityInject', window.plugin.cachePortalDetailsOnMap.entityInject);
+  window.addHook('portalDetailLoaded', window.plugin.cachePortalDetailsOnMap.portalDetailLoaded);
+  window.addHook('mapDataEntityInject', window.plugin.cachePortalDetailsOnMap.entityInject);
 };
 
-var setup =  window.plugin.cachePortalDetailsOnMap.setup;
+var setup = window.plugin.cachePortalDetailsOnMap.setup;
