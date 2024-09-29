dd90393ccc20a9b276551fb9afabae98ffbc5f88
Alexander Danilov <git@modos189.ru>
2024-09-29 19:18:11 +0500

    Fix eslint for plugins/highlight-level-color.js

--- plugins/highlight-level-color.js
+++ plugins/highlight-level-color.js
@@ -1,13 +1,16 @@
 // @author         vita10gy
 // @name           Highlight portals by level color
 // @category       Highlighter
-// @version        0.2.2
+// @version        0.2.3
 // @description    Use the portal fill color to denote the portal level by using the game level colors.
 
 /* exported setup, changelog --eslint */
-/* global COLORS_LVL */
 
 var changelog = [
+  {
+    version: '0.2.3',
+    changes: ['Refactoring: fix eslint'],
+  },
   {
     version: '0.2.2',
     changes: ['Version upgrade due to a change in the wrapper: plugin icons are now vectorized'],
@@ -18,14 +21,14 @@ var changelog = [
   },
 ];
 
-function highlightLevelColor (data) {
+function highlightLevelColor(data) {
   var portal_level = data.portal.options.data.level;
   if (portal_level !== undefined) {
-    var opacity = .6;
-    data.portal.setStyle({fillColor: COLORS_LVL[portal_level], fillOpacity: opacity});
+    var opacity = 0.6;
+    data.portal.setStyle({ fillColor: window.COLORS_LVL[portal_level], fillOpacity: opacity });
   }
 }
 
-function setup () {
+function setup() {
   window.addPortalHighlighter('Level Color', highlightLevelColor);
 }
