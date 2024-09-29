3bbbd108f460fe893d1ddf49d5890198cef21bab
Alexander Danilov <git@modos189.ru>
2024-09-29 19:19:25 +0500

    Fix eslint for plugins/highlight-portals-my-level.js

--- plugins/highlight-portals-my-level.js
+++ plugins/highlight-portals-my-level.js
@@ -1,13 +1,16 @@
 // @author         vita10gy
 // @name           Highlight portals by my level
 // @category       Highlighter
-// @version        0.2.2
+// @version        0.2.3
 // @description    Use the portal fill color to denote if the portal is either at and above, or at and below your level.
 
 /* exported setup, changelog --eslint */
-/* global PLAYER */
 
 var changelog = [
+  {
+    version: '0.2.3',
+    changes: ['Refactoring: fix eslint'],
+  },
   {
     version: '0.2.2',
     changes: ['Version upgrade due to a change in the wrapper: plugin icons are now vectorized'],
@@ -18,28 +21,27 @@ var changelog = [
   },
 ];
 
-function belowMyLevel (data) {
-  colorLevel(true,data);
+function belowMyLevel(data) {
+  colorLevel(true, data);
 }
 
-function aboveMyLevel (data) {
-  colorLevel(false,data);
+function aboveMyLevel(data) {
+  colorLevel(false, data);
 }
 
-function colorLevel (below,data) {
+function colorLevel(below, data) {
   var portal_level = data.portal.options.level;
 
   // as portal levels can never be higher than L8, clamp the player level to this for highlight purposes
-  var player_level = Math.min(PLAYER.level,8);
+  var player_level = Math.min(window.PLAYER.level, 8);
 
-  var opacity = .6;
-  if ((below && portal_level <= player_level) ||
-     (!below && portal_level >= player_level)) {
-    data.portal.setStyle({fillColor: 'red', fillOpacity: opacity});
+  var opacity = 0.6;
+  if ((below && portal_level <= player_level) || (!below && portal_level >= player_level)) {
+    data.portal.setStyle({ fillColor: 'red', fillOpacity: opacity });
   }
 }
 
-function setup () {
+function setup() {
   window.addPortalHighlighter('Below My Level', belowMyLevel);
   window.addPortalHighlighter('Above My Level', aboveMyLevel);
 }
