3ac330466eae1c8c5b4c28c144e6f835b1f14135
Alexander Danilov <git@modos189.ru>
2024-09-29 13:36:43 +0500

    Fix eslint for plugins/done-links.js

--- plugins/done-links.js
+++ plugins/done-links.js
@@ -1,12 +1,17 @@
 // @author         jonatkins
 // @name           Done links
 // @category       Draw
-// @version        0.1.3
+// @version        0.1.4
 // @description    A companion to the Cross Links plugin. Highlights any links that match existing draw-tools line/polygon edges
 
 /* exported setup, changelog --eslint */
+/* global L -- eslint */
 
 var changelog = [
+  {
+    version: '0.1.4',
+    changes: ['Refactoring: fix eslint'],
+  },
   {
     version: '0.1.3',
     changes: ['Version upgrade due to a change in the wrapper: plugin icons are now vectorized'],
@@ -17,168 +22,163 @@ var changelog = [
   },
 ];
 
-window.plugin.doneLinks = function() {};
+window.plugin.doneLinks = function () {};
 
-window.plugin.doneLinks.sameLink = function(a0, a1, b0, b1) {
+window.plugin.doneLinks.sameLink = function (a0, a1, b0, b1) {
   if (a0.equals(b0) && a1.equals(b1)) return true;
   if (a0.equals(b1) && a1.equals(b0)) return true;
   return false;
-}
+};
 
+window.plugin.doneLinks.testPolyLine = function (polyline, link, closed) {
+  var a = link.getLatLngs();
+  var b = polyline.getLatLngs();
 
-window.plugin.doneLinks.testPolyLine = function (polyline, link,closed) {
+  for (var i = 0; i < b.length - 1; ++i) {
+    if (window.plugin.doneLinks.sameLink(a[0], a[1], b[i], b[i + 1])) return true;
+  }
 
-    var a = link.getLatLngs();
-    var b = polyline.getLatLngs();
+  if (closed) {
+    if (window.plugin.doneLinks.sameLink(a[0], a[1], b[b.length - 1], b[0])) return true;
+  }
 
-    for (var i=0;i<b.length-1;++i) {
-        if (window.plugin.doneLinks.sameLink(a[0],a[1],b[i],b[i+1])) return true;
-    }
-
-    if (closed) {
-        if (window.plugin.doneLinks.sameLink(a[0],a[1],b[b.length-1],b[0])) return true;
-    }
-
-    return false;
-}
+  return false;
+};
 
 window.plugin.doneLinks.onLinkAdded = function (data) {
-    if (window.plugin.doneLinks.disabled) return;
+  if (window.plugin.doneLinks.disabled) return;
 
-    plugin.doneLinks.testLink(data.link);
-}
+  window.plugin.doneLinks.testLink(data.link);
+};
 
-window.plugin.doneLinks.checkAllLinks = function() {
-    if (window.plugin.doneLinks.disabled) return;
+window.plugin.doneLinks.checkAllLinks = function () {
+  if (window.plugin.doneLinks.disabled) return;
 
-    console.debug("Done-Links: checking all links");
-    plugin.doneLinks.linkLayer.clearLayers();
-    plugin.doneLinks.linkLayerGuids = {};
+  console.debug('Done-Links: checking all links');
+  window.plugin.doneLinks.linkLayer.clearLayers();
+  window.plugin.doneLinks.linkLayerGuids = {};
 
-    $.each(window.links, function(guid, link) {
-        plugin.doneLinks.testLink(link);
-    });
-}
+  $.each(window.links, function (guid, link) {
+    window.plugin.doneLinks.testLink(link);
+  });
+};
 
 window.plugin.doneLinks.testLink = function (link) {
-    if (plugin.doneLinks.linkLayerGuids[link.options.guid]) return;
+  if (window.plugin.doneLinks.linkLayerGuids[link.options.guid]) return;
 
-    for (var i in plugin.drawTools.drawnItems._layers) { // leaflet don't support breaking out of the loop
-       var layer = plugin.drawTools.drawnItems._layers[i];
-       if (layer instanceof L.GeodesicPolygon) {
-           if (plugin.doneLinks.testPolyLine(layer, link,true)) {
-               plugin.doneLinks.showLink(link);
-               break;
-           }
-        } else if (layer instanceof L.GeodesicPolyline) {
-            if (plugin.doneLinks.testPolyLine(layer, link)) {
-                plugin.doneLinks.showLink(link);
-                break;
-            }
-        }
-    };
-}
+  for (var i in window.plugin.drawTools.drawnItems._layers) {
+    // leaflet don't support breaking out of the loop
+    var layer = window.plugin.drawTools.drawnItems._layers[i];
+    if (layer instanceof L.GeodesicPolygon) {
+      if (window.plugin.doneLinks.testPolyLine(layer, link, true)) {
+        window.plugin.doneLinks.showLink(link);
+        break;
+      }
+    } else if (layer instanceof L.GeodesicPolyline) {
+      if (window.plugin.doneLinks.testPolyLine(layer, link)) {
+        window.plugin.doneLinks.showLink(link);
+        break;
+      }
+    }
+  }
+};
 
+window.plugin.doneLinks.showLink = function (link) {
+  var poly = L.geodesicPolyline(link.getLatLngs(), {
+    color: window.COLORS[link.options.team],
+    opacity: 0.8,
+    weight: 6,
+    interactive: false,
+    dashArray: '6,12',
 
-window.plugin.doneLinks.showLink = function(link) {
+    guid: link.options.guid,
+  });
 
-    var poly = L.geodesicPolyline(link.getLatLngs(), {
-       color: COLORS[link.options.team],
-       opacity: 0.8,
-       weight: 6,
-       interactive: false,
-       dashArray: '6,12',
-
-       guid: link.options.guid
-    });
-
-    poly.addTo(plugin.doneLinks.linkLayer);
-    plugin.doneLinks.linkLayerGuids[link.options.guid]=poly;
-}
+  poly.addTo(window.plugin.doneLinks.linkLayer);
+  window.plugin.doneLinks.linkLayerGuids[link.options.guid] = poly;
+};
 
 window.plugin.doneLinks.onMapDataRefreshEnd = function () {
-    if (window.plugin.doneLinks.disabled) return;
+  if (window.plugin.doneLinks.disabled) return;
 
-    window.plugin.doneLinks.linkLayer.bringToFront();
+  window.plugin.doneLinks.linkLayer.bringToFront();
 
-    window.plugin.doneLinks.testForDeletedLinks();
-}
+  window.plugin.doneLinks.testForDeletedLinks();
+};
 
 window.plugin.doneLinks.testAllLinksAgainstLayer = function (layer) {
-    if (window.plugin.doneLinks.disabled) return;
+  if (window.plugin.doneLinks.disabled) return;
 
-    $.each(window.links, function(guid, link) {
-        if (!plugin.doneLinks.linkLayerGuids[link.options.guid])
-        {
-            if (layer instanceof L.GeodesicPolygon) {
-                if (plugin.doneLinks.testPolyLine(layer, link,true)) {
-                    plugin.doneLinks.showLink(link);
-                }
-            } else if (layer instanceof L.GeodesicPolyline) {
-                if (plugin.doneLinks.testPolyLine(layer, link)) {
-                    plugin.doneLinks.showLink(link);
-                }
-            }
+  $.each(window.links, function (guid, link) {
+    if (!window.plugin.doneLinks.linkLayerGuids[link.options.guid]) {
+      if (layer instanceof L.GeodesicPolygon) {
+        if (window.plugin.doneLinks.testPolyLine(layer, link, true)) {
+          window.plugin.doneLinks.showLink(link);
         }
-    });
-}
+      } else if (layer instanceof L.GeodesicPolyline) {
+        if (window.plugin.doneLinks.testPolyLine(layer, link)) {
+          window.plugin.doneLinks.showLink(link);
+        }
+      }
+    }
+  });
+};
 
 window.plugin.doneLinks.testForDeletedLinks = function () {
-    window.plugin.doneLinks.linkLayer.eachLayer( function(layer) {
-        var guid = layer.options.guid;
-        if (!window.links[guid]) {
-            console.log("link removed");
-            plugin.doneLinks.linkLayer.removeLayer(layer);
-            delete plugin.doneLinks.linkLayerGuids[guid];
-        }
-    });
-}
-
-window.plugin.doneLinks.createLayer = function() {
-    window.plugin.doneLinks.linkLayer = new L.FeatureGroup();
-    window.plugin.doneLinks.linkLayerGuids={};
-    window.layerChooser.addOverlay(window.plugin.doneLinks.linkLayer, 'Done Links');
-
-    map.on('layeradd', function(obj) {
-      if(obj.layer === window.plugin.doneLinks.linkLayer) {
-        delete window.plugin.doneLinks.disabled;
-        window.plugin.doneLinks.checkAllLinks();
-      }
-    });
-    map.on('layerremove', function(obj) {
-      if(obj.layer === window.plugin.doneLinks.linkLayer) {
-        window.plugin.doneLinks.disabled = true;
-        window.plugin.doneLinks.linkLayer.clearLayers();
-        plugin.doneLinks.linkLayerGuids = {};
-      }
-    });
-
-    // ensure 'disabled' flag is initialised
-    if (!map.hasLayer(window.plugin.doneLinks.linkLayer)) {
-        window.plugin.doneLinks.disabled = true;
+  window.plugin.doneLinks.linkLayer.eachLayer(function (layer) {
+    var guid = layer.options.guid;
+    if (!window.links[guid]) {
+      console.log('link removed');
+      window.plugin.doneLinks.linkLayer.removeLayer(layer);
+      delete window.plugin.doneLinks.linkLayerGuids[guid];
     }
-}
+  });
+};
 
-var setup = function() {
-    if (window.plugin.drawTools === undefined) {
-       alert("'Done-Links' requires 'draw-tools'");
-       return;
+window.plugin.doneLinks.createLayer = function () {
+  window.plugin.doneLinks.linkLayer = new L.FeatureGroup();
+  window.plugin.doneLinks.linkLayerGuids = {};
+  window.layerChooser.addOverlay(window.plugin.doneLinks.linkLayer, 'Done Links');
+
+  window.map.on('layeradd', function (obj) {
+    if (obj.layer === window.plugin.doneLinks.linkLayer) {
+      delete window.plugin.doneLinks.disabled;
+      window.plugin.doneLinks.checkAllLinks();
     }
+  });
+  window.map.on('layerremove', function (obj) {
+    if (obj.layer === window.plugin.doneLinks.linkLayer) {
+      window.plugin.doneLinks.disabled = true;
+      window.plugin.doneLinks.linkLayer.clearLayers();
+      window.plugin.doneLinks.linkLayerGuids = {};
+    }
+  });
 
-    window.plugin.doneLinks.createLayer();
+  // ensure 'disabled' flag is initialised
+  if (!window.map.hasLayer(window.plugin.doneLinks.linkLayer)) {
+    window.plugin.doneLinks.disabled = true;
+  }
+};
 
-    // events
-    window.addHook('pluginDrawTools',function(e) {
-        if (e.event == 'layerCreated') {
-            // we can just test the new layer in this case
-            window.plugin.doneLinks.testAllLinksAgainstLayer(e.layer);
-        } else {
-            // all other event types - assume anything could have been modified and re-check all links
-            window.plugin.doneLinks.checkAllLinks();
-        }
-    });
+var setup = function () {
+  if (window.plugin.drawTools === undefined) {
+    alert("'Done-Links' requires 'draw-tools'");
+    return;
+  }
 
-    window.addHook('linkAdded', window.plugin.doneLinks.onLinkAdded);
-    window.addHook('mapDataRefreshEnd', window.plugin.doneLinks.onMapDataRefreshEnd);
+  window.plugin.doneLinks.createLayer();
 
-}
+  // events
+  window.addHook('pluginDrawTools', function (e) {
+    if (e.event === 'layerCreated') {
+      // we can just test the new layer in this case
+      window.plugin.doneLinks.testAllLinksAgainstLayer(e.layer);
+    } else {
+      // all other event types - assume anything could have been modified and re-check all links
+      window.plugin.doneLinks.checkAllLinks();
+    }
+  });
+
+  window.addHook('linkAdded', window.plugin.doneLinks.onLinkAdded);
+  window.addHook('mapDataRefreshEnd', window.plugin.doneLinks.onMapDataRefreshEnd);
+};
