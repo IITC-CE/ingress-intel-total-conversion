e731144c58cf3febba3576fc21fbb6190dc00b88
Alexander Danilov <git@modos189.ru>
2024-10-30 12:22:57 +0500

    Added `String.prototype.padStart` polyfill

--- core/code/utils_polyfills.js
+++ core/code/utils_polyfills.js
@@ -94,3 +94,26 @@ if (!Element.prototype.closest) {
     return null;
   };
 }
+
+// https://github.com/KhaledElAnsari/String.prototype.padStart/blob/master/index.js
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function (targetLength, padString) {
+    targetLength = Math.floor(targetLength) || 0;
+    if (targetLength < this.length) return String(this);
+
+    padString = padString ? String(padString) : ' ';
+
+    var pad = '';
+    var len = targetLength - this.length;
+    var i = 0;
+    while (pad.length < len) {
+      if (!padString[i]) {
+        i = 0;
+      }
+      pad += padString[i];
+      i++;
+    }
+
+    return pad + String(this).slice(0);
+  };
+}
