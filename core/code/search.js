3e62361eacfabdce65c00e2eb8be09431c431ea0
Alexander Danilov <git@modos189.ru>
2024-11-02 20:37:21 +0500

    Refactoring of IITC.search

--- core/code/search.js
+++ core/code/search.js
@@ -24,7 +24,8 @@
  *   May return `true` to prevent the map from being repositioned. You may reposition the map yourself or do other work.
  * - `onRemove(result)`: A handler to be called when the result is removed from the map
  *   (because another result has been selected or the search was cancelled by the user).
- *  @namespace window.search
+ *  @namespace IITC.search
+ *  @memberof IITC
  */
 IITC.search = {
   lastSearch: null,
@@ -33,66 +34,76 @@ IITC.search = {
 /**
  * Initiates a search with the specified term and confirmation status.
  *
- * @function window.search.doSearch
+ * @function IITC.search.doSearch
  * @param {string} term - The search term.
  * @param {boolean} confirmed - Indicates if the search term is confirmed.
  */
 IITC.search.doSearch = function (term, confirmed) {
-  term = term.trim();
+  const searchTerm = term.trim();
 
-  // minimum 3 characters for automatic search
-  if (term.length < 3 && !confirmed) return;
+  // Minimum 3 characters for automatic search
+  if (searchTerm.length < 3 && !confirmed) return;
 
-  // don't clear last confirmed search
-  if (IITC.search.lastSearch && IITC.search.lastSearch.confirmed && !confirmed) return;
+  // Avoid clearing last confirmed search
+  const lastSearch = IITC.search.lastSearch;
+  if (lastSearch?.confirmed && !confirmed) return;
 
-  // don't make the same query again
-  if (IITC.search.lastSearch && IITC.search.lastSearch.confirmed === confirmed && IITC.search.lastSearch.term === term) return;
+  // Prevent repeat of identical query
+  if (lastSearch?.confirmed === confirmed && lastSearch.term === searchTerm) return;
 
-  if (IITC.search.lastSearch) IITC.search.lastSearch.hide();
+  if (lastSearch) lastSearch.hide();
   IITC.search.lastSearch = null;
 
-  // clear results
-  if (term === '') return;
+  if (searchTerm === '') return;
 
   if (window.useAppPanes()) window.show('info');
 
-  $('.ui-tooltip').remove();
+  document.querySelectorAll('.ui-tooltip').forEach((tooltip) => tooltip.remove());
 
-  IITC.search.lastSearch = new IITC.search.Query(term, confirmed);
+  IITC.search.lastSearch = new IITC.search.Query(searchTerm, confirmed);
   IITC.search.lastSearch.show();
 };
 
 /**
  * Sets up the search input field and button functionality.
  *
- * @function window.search.setup
+ * @function IITC.search.setup
  */
 IITC.search.setup = function () {
-  $('#search')
-    .keypress(function (e) {
-      if ((e.keyCode ? e.keyCode : e.which) !== 13) return;
+  const searchInput = document.querySelector('#search');
+  const geoLocationButton = document.querySelector('#buttongeolocation');
+  let searchTimer;
+
+  if (searchInput) {
+    searchInput.addEventListener('keydown', (e) => {
+      if (e.key !== 'Enter') return;
       e.preventDefault();
 
-      const term = $(this).val();
-
-      clearTimeout(IITC.search.timer);
+      const term = searchInput.value.trim();
+      clearTimeout(searchTimer);
       IITC.search.doSearch(term, true);
-    })
-    .on('keyup keypress change paste', function () {
-      clearTimeout(IITC.search.timer);
-      IITC.search.timer = setTimeout(() => {
-        const term = $(this).val();
-        IITC.search.doSearch(term, false);
-      }, 500);
     });
-  $('#buttongeolocation').click(() => window.map.locate({ setView: true, maxZoom: 13 }));
+
+    searchInput.addEventListener('input', () => {
+      clearTimeout(searchTimer);
+      searchTimer = setTimeout(() => {
+        const term = searchInput.value.trim();
+        IITC.search.doSearch(term, false);
+      }, 100);
+    });
+  }
+
+  if (geoLocationButton) {
+    geoLocationButton.addEventListener('click', () => {
+      window.map.locate({ setView: true, maxZoom: 13 });
+    });
+  }
 };
 
 /**
  * Adds a search result for a portal to the search query results.
  *
- * @function window.search.addSearchResult
+ * @function IITC.search.addSearchResult
  * @param {Object} query - The search query object to which the result will be added.
  * @param {Object} data - The data for the search result. This includes information such as title, team, level, health, etc.
  * @param {string} guid - GUID if the portal.
@@ -101,17 +112,21 @@ IITC.search.addSearchResult = function (query, data, guid) {
   const team = window.teamStringToId(data.team);
   const color = team === window.TEAM_NONE ? '#CCC' : window.COLORS[team];
   const latLng = L.latLng(data.latE6 / 1e6, data.lngE6 / 1e6);
+
   query.addResult({
     title: data.title,
     description: `${window.TEAM_SHORTNAMES[team]}, L${data.level}, ${data.health}%, ${data.resCount} Resonators`,
     position: latLng,
     icon: `data:image/svg+xml;base64,${btoa('@include_string:images/icon-portal.svg@'.replace(/%COLOR%/g, color))}`,
+
     onSelected(result, event) {
+      const { position } = result;
+
       if (event.type === 'dblclick') {
         window.zoomToAndShowPortal(guid, latLng);
       } else if (window.portals[guid]) {
-        if (!window.map.getBounds().contains(result.position)) {
-          window.map.setView(result.position);
+        if (!window.map.getBounds().contains(position)) {
+          window.map.setView(position);
         }
         window.renderPortalDetails(guid);
       } else {
