bbcca0026373ba008951def49bcff1458c1228f8
Alexander Danilov <git@modos189.ru>
2025-08-05 14:32:47 +0500

    feat: add support for iitc:// custom scheme deep links

--- mobile/app/src/main/java/org/exarhteam/iitc_mobile/IITC_Mobile.java
+++ mobile/app/src/main/java/org/exarhteam/iitc_mobile/IITC_Mobile.java
@@ -453,6 +453,14 @@ public class IITC_Mobile extends AppCompatActivity
                     return;
                 }
             }
+            
+            if (uri.getScheme().equals("iitc")) {
+                // Convert iitc:// scheme to https://intel.ingress.com/ URL
+                String convertedUrl = mIntelUrl + uri.getSchemeSpecificPart();
+                Log.d("loading iitc deep link: " + uri.toString() + " -> " + convertedUrl);
+                loadUrl(convertedUrl);
+                return;
+            }
 
             if (uri.getScheme().equals("geo")) {
                 try {
