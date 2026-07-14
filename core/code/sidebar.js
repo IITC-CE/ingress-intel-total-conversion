cdbf1ee258fd115df0b1dd8b26238838cedf97b4
Alexander Danilov <git@modos189.ru>
2026-07-14 14:37:58 +0500

    chore(core): rename portal_detail_display.js to portal_display.js

--- core/code/sidebar.js
+++ core/code/sidebar.js
@@ -52,7 +52,7 @@ function setupIcons() {
       '<svg>',
       // Material Icons
 
-      // portal_detail_display.js
+      // portal_display.js
       '<symbol id="ic_place_24px" viewBox="0 0 24 24">',
       '<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 0 1 0-5 2.5 2.5 0 0 1 0 5z"/>',
       '</symbol>',
