74b3911d972e4712a908dfdc516a180ed5a4da63
McBen <viertelvor12@gmx.net>
2026-09-17 16:17:04 +0200

    build: add css minimizer

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssImport from 'postcss-import';
 import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 
 export default {
-  plugins: [postcssImport(), postcssPresetEnv(), autoprefixer()],
+  plugins: [postcssImport(), postcssPresetEnv(), autoprefixer(), cssnano()],
 };
